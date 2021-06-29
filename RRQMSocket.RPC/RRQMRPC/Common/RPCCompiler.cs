8d8eb43321ebb050d05f2b8b733e08a282b161e2
若汝棋茗 <1751615839@qq.com>
2021-06-29 09:07:05 +0800

    增加RPCCompiler

---
+++ RRQMSocket.RPC/RRQMRPC/Common/RPCCompiler.cs
@@ -0,0 +1,63 @@
+﻿#if NET45_OR_GREATER
+using Microsoft.CSharp;
+using RRQMCore.Log;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RRQMSocket.RPC.RRQMRPC
+{
+    /// <summary>
+    /// RPC编译器
+    /// </summary>
+    public class RPCCompiler : IRPCCompiler
+    {
+        /// <summary>
+        /// 编译代码
+        /// </summary>
+        /// <param name="assemblyName"></param>
+        /// <param name="codes"></param>
+        /// <param name="refStrings"></param>
+        /// <returns></returns>
+        public byte[] CompileCode(string assemblyName, string[] codes, List<string> refStrings)
+        {
+            CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+            CompilerParameters compilerParameters = new CompilerParameters();
+
+            compilerParameters.OutputAssembly = assemblyName;
+
+            compilerParameters.GenerateExecutable = false;
+            compilerParameters.GenerateInMemory = false;
+            compilerParameters.ReferencedAssemblies.Add("System.dll");
+            compilerParameters.ReferencedAssemblies.Add("mscorlib.dll");
+            compilerParameters.ReferencedAssemblies.Add(typeof(ILog).Assembly.Location);
+            compilerParameters.ReferencedAssemblies.Add(typeof(RPCService).Assembly.Location);
+
+            foreach (var item in refStrings)
+            {
+                compilerParameters.ReferencedAssemblies.Add(item);
+            }
+
+            CompilerResults cr = codeProvider.CompileAssemblyFromSource(compilerParameters, codes);
+            if (cr.Errors.HasErrors)
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+
+                foreach (CompilerError err in cr.Errors)
+                {
+                    stringBuilder.AppendLine(err.ErrorText);
+                }
+
+                throw new RRQMRPCException(stringBuilder.ToString());
+            }
+
+            return File.ReadAllBytes(assemblyName);
+        }
+
+    }
+}
+#endif
