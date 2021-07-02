3a5d794ff072667cd2bfb210eda28e7e23580f7a
若汝棋茗 <505554090@qq.com>
2021-07-03 00:29:10 +0800

    增加：XmlRpcClient

--- RRQMSocket.RPC.XmlRpc/Parser/XmlRpcParser.cs
+++ RRQMSocket.RPC.XmlRpc/Parser/XmlRpcParser.cs
@@ -71,31 +71,7 @@ namespace RRQMSocket.RPC.XmlRpc
             httpResponse.ProtocolVersion = httpRequest.ProtocolVersion;
             ByteBlock byteBlock = this.BytePool.GetByteBlock(this.BufferLength);
 
-            XmlDocument xml = new XmlDocument();
-
-            XmlDeclaration xmlDecl = xml.CreateXmlDeclaration("1.0", "utf-8", string.Empty);
-            xml.AppendChild(xmlDecl);
-
-            XmlElement xmlElement = xml.CreateElement("methodResponse");
-
-            xml.AppendChild(xmlElement);
-
-            XmlElement paramsElement = xml.CreateElement("params");
-            xmlElement.AppendChild(paramsElement);
-
-            XmlElement paramElement = xml.CreateElement("param");
-            paramsElement.AppendChild(paramElement);
-
-            XmlElement valueElement = xml.CreateElement("value");
-            paramElement.AppendChild(valueElement);
-
-            CreatResponse(xml, valueElement, methodInvoker.ReturnParameter);
-            ByteBlock xmlBlock = this.BytePool.GetByteBlock(this.BufferLength);
-            xml.Save(xmlBlock);
-
-            string xmlString = Encoding.UTF8.GetString(xmlBlock.Buffer, 0, (int)xmlBlock.Position);
-
-            httpResponse.FromXML(xmlString);
+            XmlDataTool.CreatResponse(httpResponse, methodInvoker.ReturnParameter);
             try
             {
                 httpResponse.Build(byteBlock);
@@ -129,7 +105,7 @@ namespace RRQMSocket.RPC.XmlRpc
                         {
                             throw new RRQMRPCException("XmlRpc服务中不允许有out及ref关键字");
                         }
-                        string actionKey = string.IsNullOrEmpty(attribute.ActionKey) ? $"{methodInstance.Provider.GetType().Name}.{methodInstance.Method.Name}" : attribute.ActionKey;
+                        string actionKey = string.IsNullOrEmpty(attribute.ActionKey) ? $"{methodInstance.Method.Name}" : attribute.ActionKey;
 
                         this.actionMap.Add(actionKey, methodInstance);
                     }
@@ -175,148 +151,11 @@ namespace RRQMSocket.RPC.XmlRpc
             {
                 socketClient.OnReceived = this.OnReceived;
             }
-            socketClient.DataHandlingAdapter = new HttpDataHandlingAdapter(this.BufferLength);
+            socketClient.SetDataHandlingAdapter( new HttpDataHandlingAdapter(this.BufferLength, HttpType.Server));
         }
 
-        private void CreatResponse(XmlDocument xml, XmlNode xmlNode, object value)
-        {
-            if (value is int)
-            {
-                XmlElement valueElement = xml.CreateElement("i4");
-                valueElement.InnerText = value.ToString();
-                xmlNode.AppendChild(valueElement);
-            }
-            else if (value is bool)
-            {
-                XmlElement valueElement = xml.CreateElement("boolean");
-                valueElement.InnerText = ((int)value).ToString();
-                xmlNode.AppendChild(valueElement);
-            }
-            else if (value is double)
-            {
-                XmlElement valueElement = xml.CreateElement("double");
-                valueElement.InnerText = ((double)value).ToString();
-                xmlNode.AppendChild(valueElement);
-            }
-            else if (value is string)
-            {
-                XmlElement valueElement = xml.CreateElement("string");
-                valueElement.InnerText = value.ToString();
-                xmlNode.AppendChild(valueElement);
-            }
-            else if (value is DateTime)
-            {
-                XmlElement valueElement = xml.CreateElement("dateTime.iso8601");
-                valueElement.InnerText = ((DateTime)value).ToString();
-                xmlNode.AppendChild(valueElement);
-            }
-            else if (value is byte[])
-            {
-                XmlElement valueElement = xml.CreateElement("base64");
-                string str = Convert.ToBase64String((byte[])value);
-                valueElement.InnerText = str;
-                xmlNode.AppendChild(valueElement);
-            }
-            else if (value.GetType().IsArray)
-            {
-                Array array = (Array)value;
-                XmlElement arrayElement;
-
-                arrayElement = xml.CreateElement("array");
-
-                xmlNode.AppendChild(arrayElement);
-
-                XmlElement dataElememt = xml.CreateElement("data");
-                arrayElement.AppendChild(dataElememt);
-
-                foreach (var item in array)
-                {
-                    XmlElement valueElement = xml.CreateElement("value");
-                    dataElememt.AppendChild(valueElement);
-                    CreatResponse(xml, valueElement, item);
-                }
-            }
-            else
-            {
-                XmlElement valueElement = xml.CreateElement("struct");
-                xmlNode.AppendChild(valueElement);
-
-                PropertyInfo[] propertyInfos = value.GetType().GetProperties();
-                foreach (var propertyInfo in propertyInfos)
-                {
-                    XmlElement memberElement = xml.CreateElement("member");
-                    valueElement.AppendChild(memberElement);
-
-                    XmlElement nameElement = xml.CreateElement("name");
-                    nameElement.InnerText = propertyInfo.Name;
-                    memberElement.AppendChild(nameElement);
-
-                    XmlElement oValueElement = xml.CreateElement("value");
-                    memberElement.AppendChild(oValueElement);
-
-                    object oValue = propertyInfo.GetValue(value);
-                    this.CreatResponse(xml, oValueElement, oValue);
-                }
-            }
-        }
-
-        private object GetValue(XmlNode valueNode, Type type)
-        {
-            switch (valueNode.Name)
-            {
-                case "boolean":
-                    {
-                        return bool.Parse(valueNode.InnerText);
-                    }
-                case "i4":
-                case "int":
-                    {
-                        return int.Parse(valueNode.InnerText);
-                    }
-                case "double":
-                    {
-                        return double.Parse(valueNode.InnerText);
-                    }
-                case "dateTime.iso8601":
-                    {
-                        return DateTime.Parse(valueNode.InnerText);
-                    }
-                case "base64":
-                    {
-                        return valueNode.InnerText;
-                    }
-                case "struct":
-                    {
-                        object instance = Activator.CreateInstance(type);
-                        foreach (XmlNode memberNode in valueNode.ChildNodes)
-                        {
-                            string name = memberNode.SelectSingleNode("name").InnerText;
-                            PropertyInfo property = type.GetProperty(name);
-                            property.SetValue(instance, this.GetValue(memberNode.SelectSingleNode("value").FirstChild, property.PropertyType));
-                        }
-                        return instance;
-                    }
-                case "arrays":
-                case "array":
-                    {
-                        XmlNode dataNode = valueNode.SelectSingleNode("data");
-                        Array array = Array.CreateInstance(type.GetElementType(), dataNode.ChildNodes.Count);
-
-                        int index = 0;
-                        foreach (XmlNode arrayValueNode in dataNode.ChildNodes)
-                        {
-                            array.SetValue(this.GetValue(arrayValueNode.FirstChild, type.GetElementType()), index);
-                            index++;
-                        }
-                        return array;
-                    }
-                default:
-                case "string":
-                    {
-                        return valueNode.InnerText;
-                    }
-            }
-        }
+       
+      
 
         private void OnReceived(SimpleSocketClient socketClient, ByteBlock byteBlock, object obj)
         {
@@ -342,7 +181,7 @@ namespace RRQMSocket.RPC.XmlRpc
                         foreach (XmlNode paramNode in paramsNode.ChildNodes)
                         {
                             XmlNode valueNode = paramNode.FirstChild.FirstChild;
-                            ps.Add(this.GetValue(valueNode, methodInstance.ParameterTypes[index]));
+                            ps.Add(XmlDataTool.GetValue(valueNode, methodInstance.ParameterTypes[index]));
                             index++;
                         }
 
