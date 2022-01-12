4d272b7859eda248cb940bfb656c8a51269dd452
若汝棋茗 <505554090@qq.com>
2022-01-12 19:41:24 +0800

    修复资源文件

--- RRQMCore/Resource.Designer.cs
+++ RRQMCore/Resource.Designer.cs
@@ -39,7 +39,7 @@ namespace RRQMCore {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RRQMSocketFramework.RRQMCore.Resource", typeof(Resource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RRQMCore.Resource", typeof(Resource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
