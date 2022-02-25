6dfed30fd13f43c1fed0386cfd59a8271a2a448c
若汝棋茗 <505554090@qq.com>
2022-02-25 11:03:04 +0800

    优化线程安全队列

--- RRQMCore/Collections/Concurrent/IntelligentDataQueue.cs
+++ RRQMCore/Collections/Concurrent/IntelligentDataQueue.cs
@@ -156,4 +156,15 @@ namespace RRQMCore.Collections.Concurrent
             return this.actualSize < this.maxSize;
         }
     }
+
+    /// <summary>
+    /// 队列数据
+    /// </summary>
+    public interface IQueueData
+    {
+        /// <summary>
+        /// 数据长度
+        /// </summary>
+        int Size { get; }
+    }
 }
