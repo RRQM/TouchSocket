aac929e9bdec633d1d53598a4a68d01d9b76d52a
若汝棋茗 <505554090@qq.com>
2022-07-29 23:59:09 +0800

    更新demo

--- examples/NAT简单示例/NATServiceConsoleApp/Program.cs
+++ examples/NAT简单示例/NATServiceConsoleApp/Program.cs
@@ -3,7 +3,9 @@ using TouchSocket.Core;
 using TouchSocket.Core.ByteManager;
 using TouchSocket.Core.Config;
 using TouchSocket.Core.Log;
+using TouchSocket.Core.Plugins;
 using TouchSocket.Sockets;
+using TouchSocket.Sockets.Plugins;
 
 namespace NATServiceConsoleApp
 {
@@ -18,7 +20,7 @@ namespace NATServiceConsoleApp
             service.Setup(config);
             service.Start();
 
-            Console.WriteLine("转发服务器已启动。已将7788端口转发到127.0.0.1:7789与127.0.0.1:7790地址");
+            Console.WriteLine("转发服务器已启动。已将7788端口转发到127.0.0.1:7789地址");
         }
     }
 
@@ -32,8 +34,15 @@ namespace NATServiceConsoleApp
             {
                 //此处模拟的是只要连接到NAT服务器，就转发。
                 //实际上，这个方法可以随时调用。
-                socketClient.AddTargetClient(new TouchSocketConfig().SetRemoteIPHost("127.0.0.1:7789"));
-                socketClient.AddTargetClient(new TouchSocketConfig().SetRemoteIPHost("127.0.0.1:7790"));
+                socketClient.AddTargetClient(new TouchSocketConfig()
+                    .SetRemoteIPHost("127.0.0.1:7789")
+                    .ConfigurePlugins(a=> 
+                    {
+                        //在企业版中，使用以下任意方式，可实现转发客户端的断线重连。
+                        a.Add<PollingKeepAlivePlugin<TcpClient>>()
+                        .SetTick(1000);//每秒检查
+                        //a.UseReconnection();
+                    }));
             }
             catch (Exception ex)
             {
