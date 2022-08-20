0fb1e650c76cca4ad328a982b845226a8dbb97c7
若汝棋茗 <505554090@qq.com>
2022-08-20 23:44:16 +0800

    修改demo

--- examples/TouchRpc简单示例/TouchRpcServerApp/Program.cs
+++ examples/TouchRpc简单示例/TouchRpcServerApp/Program.cs
@@ -17,76 +17,76 @@ namespace TouchRpcServerApp
     {
         private static void Main(string[] args)
         {
-            RpcStore rpcStore = new RpcStore(new TouchSocket.Core.Dependency.Container());
-            rpcStore.ProxyUrl = "/proxy";//代理url
-            rpcStore.OnRequestProxy = (request) =>//此处做请求验证，保证代理文件安全。
-            {
-                if (request.TryGetQuery("token", out string value) && value == "123")
-                {
-                    return true;
-                }
-                return false;
-            };
-
-            rpcStore.AddRpcParser("tcpTouchRpcParser", CreateTcpParser());
-            rpcStore.AddRpcParser("httpTouchRpcParser", CreateHttpParser());
-            rpcStore.AddRpcParser("udpTouchRpcParser", CreateUdpParser());
-
-            //注册当前程序集的所有服务
-            rpcStore.RegisterServer<MyRpcServer>();
-
-            //分享代理，代理文件可通过RRQMTool，或者浏览器获取，远程获取。
-            //http://127.0.0.1:8848/proxy?proxy=all&token=123
-            rpcStore.ShareProxy(new IPHost(8848));
-
-            //或者直接本地导出代理文件。
-            ServerCellCode[] codes = rpcStore.GetProxyInfo(RpcStore.ProxyAttributeMap.Values.ToArray());
-            string codeString = CodeGenerator.ConvertToCode("RRQMProxy", codes);
-            Console.WriteLine(codeString);
-
-            Console.WriteLine("服务器已启动");
+            TcpTouchRpcService tcpTouchRpcService= CreateTcpTouchRpcService(7789);
+            HttpTouchRpcService httpTouchRpcService = CreateHttpTouchRpcService(7790);
+            UdpTouchRpc udpTouchRpc = CreateUdpTouchRpc(7791);
             Console.ReadKey();
         }
 
-        /// <summary>
-        /// 解析器是实际的逻辑服务器，此处是<see cref="TcpTouchRpcService"/>。
-        /// </summary>
-        /// <returns></returns>
-        private static IRpcParser CreateTcpParser()
+     
+        private static TcpTouchRpcService CreateTcpTouchRpcService(int port)
         {
-            return new TouchSocketConfig()//配置
-                   .SetListenIPHosts(new IPHost[] { new IPHost(7789) })
-                   .SetMaxCount(10000)
-                   .SetThreadCount(100)
-                   .SetVerifyToken("TouchRpc")
-                   .BuildWithTcpTouchRpcService();//此处build相当于new TcpTouchRpcService，然后Setup，然后Start。
+            var service =new  TcpTouchRpcService();
+            TouchSocketConfig config=  new TouchSocketConfig()//配置
+                   .SetListenIPHosts(new IPHost[] { new IPHost(port) })
+                   .ConfigureContainer(a=> 
+                   {
+                       a.SetLogger<LoggerGroup<ConsoleLogger, FileLogger>>();//注册一个日志组
+                   })
+                   .ConfigureRpcStore(a=> 
+                   {
+                       a.RegisterServer<MyRpcServer>();//注册服务
+                   })
+                   .SetVerifyToken("TouchRpc");
+           
+            service.Setup(config)
+                .Start();
+
+            service.Logger.Info($"{service.GetType().Name}已启动，监听端口：{port}");
+            return service;
         }
 
-        /// <summary>
-        /// 此处是<see cref="HttpTouchRpcService"/>。
-        /// </summary>
-        /// <returns></returns>
-        private static IRpcParser CreateHttpParser()
+        private static HttpTouchRpcService CreateHttpTouchRpcService(int port)
         {
-            return new TouchSocketConfig()//配置
-                   .SetListenIPHosts(new IPHost[] { new IPHost(7790) })
-                   .SetMaxCount(10000)
-                   .SetThreadCount(100)
-                   .SetVerifyToken("TouchRpc")
-                   .BuildWithHttpTouchRpcService();//此处build相当于new HttpTouchRpcService，然后Setup，然后Start。
+            var service = new HttpTouchRpcService();
+            TouchSocketConfig config = new TouchSocketConfig()//配置
+                   .SetListenIPHosts(new IPHost[] { new IPHost(port) })
+                   .ConfigureContainer(a =>
+                   {
+                       a.SetLogger<LoggerGroup<ConsoleLogger, FileLogger>>();//注册一个日志组
+                   })
+                   .ConfigureRpcStore(a =>
+                   {
+                       a.RegisterServer<MyRpcServer>();//注册服务
+                   })
+                   .SetVerifyToken("TouchRpc");
+
+            service.Setup(config)
+                .Start();
+
+            service.Logger.Info($"{service.GetType().Name}已启动，监听端口：{port}");
+            return service;
         }
 
-        /// <summary>
-        /// 此处是<see cref="UdpTouchRpc"/>。
-        /// </summary>
-        /// <returns></returns>
-        private static IRpcParser CreateUdpParser()
+        private static UdpTouchRpc CreateUdpTouchRpc(int port)
         {
-            TouchSocketConfig config = new TouchSocketConfig();
-            return config.SetBindIPHost(7791)
-                 .SetBufferLength(1024 * 10)
-                 .SetThreadCount(10)
-                 .BuildWithUdpTouchRpc();
+            var service = new UdpTouchRpc();
+            TouchSocketConfig config = new TouchSocketConfig()//配置
+                   .SetBindIPHost( new IPHost(port))
+                   .ConfigureContainer(a =>
+                   {
+                       a.SetLogger<LoggerGroup<ConsoleLogger, FileLogger>>();//注册一个日志组
+                   })
+                   .ConfigureRpcStore(a =>
+                   {
+                       a.RegisterServer<MyRpcServer>();//注册服务
+                   });
+
+            service.Setup(config)
+                .Start();
+
+            service.Logger.Info($"{service.GetType().Name}已启动，监听端口：{port}");
+            return service;
         }
     }
 
