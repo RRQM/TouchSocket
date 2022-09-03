81a2dab2fa096b8fd3b781bded00aac7b48497d4
若汝棋茗 <505554090@qq.com>
2022-09-03 21:59:42 +0800

    新增示例

---
+++ examples/BytePool简单示例/BytePoolConsoleApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using TouchSocket.Core.ByteManager;
+using TouchSocket.Core.Diagnostics;
+
+namespace BytePoolConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+
+            BytePool.AddSizeKey(1024*1024);
+            //BytePool.AutoZero = true;
+            for (int i = 0; i < 5; i++)
+            {
+                byte[] data = BytePool.GetByteCore(1024*10,true);
+                BytePool.Recycle(data);
+                using (ByteBlock byteBlock = new ByteBlock(1024 * 10, true))
+                {
+                    //最重要：千万不要引用byteBlock.Buffer
+                    byteBlock.Write(10);
+                    byteBlock.Write('A');
+                    byteBlock.Write(100L);
+                    byteBlock.Write(3.1415926);
+                    byteBlock.Write("Hello TouchSocket");
+
+                   var buffer= byteBlock.ToArray();
+
+                    byteBlock.Position = 0;
+
+                    var p1 = byteBlock.ReadInt32();
+                    var p2 = byteBlock.ReadChar();
+                    var p3 = byteBlock.ReadInt64();
+                    var p4 = byteBlock.ReadDouble();
+                    var p5 = byteBlock.ReadString();
+                }
+            }
+           
+            Console.ReadKey();
+        }
+
+        static void Performance()
+        {
+            int count = 1000000;
+            TimeSpan timeSpan1 = TimeMeasurer.Run(() =>
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    byte[] buffer = new byte[1024];
+                }
+            });
+
+            TimeSpan timeSpan2 = TimeMeasurer.Run(() =>
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    ByteBlock byteBlock = new ByteBlock(1024, true);
+                    byteBlock.Dispose();
+                }
+            });
+
+            Console.WriteLine($"直接实例化：{timeSpan1}");
+            Console.WriteLine($"内存池实例化：{timeSpan2}");
+        }
+    }
+}
