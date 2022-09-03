6f57f34e31178b3b09386318afc525b139dc8eec
若汝棋茗 <505554090@qq.com>
2022-09-03 09:07:14 +0800

    更新引用

--- examples/FileTransfer简单示例/FileClientApp/FileServiceConsoleApp/Program.cs
+++ examples/FileTransfer简单示例/FileClientApp/FileServiceConsoleApp/Program.cs
@@ -6,6 +6,7 @@ using TouchSocket.Core.Plugins;
 using TouchSocket.Rpc.TouchRpc;
 using TouchSocket.Rpc.TouchRpc.Plugins;
 using TouchSocket.Sockets;
+using TouchSocket.Sockets.Plugins;
 
 namespace FileServiceConsoleApp
 {
@@ -27,9 +28,10 @@ namespace FileServiceConsoleApp
                 {
                     a.SetSingletonLogger<LoggerGroup<ConsoleLogger, FileLogger>>();
                 })
-                .ConfigurePlugins(a=> 
+                .ConfigurePlugins(a =>
                 {
                     a.Add<MyPlugin>();
+                    a.Add<MyTcpPlugin>();
                 })
                 .SetVerifyToken("File")//连接验证口令。
                 .BuildWithTcpTouchRpcService();//此处build相当于new TcpTouchRpcService，然后Setup，然后Start。
@@ -38,28 +40,30 @@ namespace FileServiceConsoleApp
         }
     }
 
-    class MyPlugin:TouchRpcPluginBase
+    class MyPlugin : TouchRpcPluginBase<TcpTouchRpcSocketClient>
     {
-
-        protected override void OnFileTransfering(ITouchRpc client, FileOperationEventArgs e)
+        protected override void OnFileTransfering(TcpTouchRpcSocketClient client, FileOperationEventArgs e)
         {
             //有可能是上传，也有可能是下载
-            client.Logger.Info($"有客户端请求传输文件，ID={((TcpTouchRpcSocketClient)client).ID}，请求类型={e.TransferType}，请求文件名={e.FileRequest.Path}");
+            client.Logger.Info($"有客户端请求传输文件，ID={client.ID}，请求类型={e.TransferType}，请求文件名={e.FileRequest.Path}");
             base.OnFileTransfering(client, e);
         }
 
-        protected override void OnFileTransfered(ITouchRpc client, FileTransferStatusEventArgs e)
+        protected override void OnFileTransfered(TcpTouchRpcSocketClient client, FileTransferStatusEventArgs e)
         {
             //传输结束，但是不一定成功，需要从e.Result判断状态。
-            client.Logger.Info($"客户端传输文件结束，ID={((TcpTouchRpcSocketClient)client).ID}，请求类型={e.TransferType}，文件名={e.FileRequest.Path}，请求状态={e.Result}");
+            client.Logger.Info($"客户端传输文件结束，ID={client.ID}，请求类型={e.TransferType}，文件名={e.FileRequest.Path}，请求状态={e.Result}");
             base.OnFileTransfered(client, e);
         }
-        protected override void OnHandshaked(ITouchRpc client, MsgEventArgs e)
+        protected override void OnHandshaked(TcpTouchRpcSocketClient client, MsgEventArgs e)
         {
-            client.Logger.Info($"有客户端成功验证，ID={((TcpTouchRpcSocketClient)client).ID}");
+            client.Logger.Info($"有客户端成功验证，ID={client.ID}");
             base.OnHandshaked(client, e);
         }
+    }
 
+    class MyTcpPlugin : TcpPluginBase
+    {
         protected override void OnDisconnected(ITcpClientBase client, ClientDisconnectedEventArgs e)
         {
             client.Logger.Info($"有客户端断开，ID={((TcpTouchRpcSocketClient)client).ID}");
