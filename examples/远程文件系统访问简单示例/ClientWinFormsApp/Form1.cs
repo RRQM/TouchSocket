fae9cb49d1e05dd86841789381b8d00170af48cb
若汝棋茗 <505554090@qq.com>
2022-09-03 10:20:51 +0800

    增加文件访问demo

--- examples/远程文件系统访问简单示例/ClientWinFormsApp/Form1.cs
+++ examples/远程文件系统访问简单示例/ClientWinFormsApp/Form1.cs
@@ -7,6 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TouchSocket.Core;
+using TouchSocket.Core.Config;
+using TouchSocket.Core.Dependency;
+using TouchSocket.Core.Log;
+using TouchSocket.Rpc.TouchRpc;
+using TouchSocket.Sockets;
 
 namespace ClientWinFormsApp
 {
@@ -15,7 +21,115 @@ namespace ClientWinFormsApp
         public Form1()
         {
             InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
         }
 
+        TcpTouchRpcClient m_client = new TcpTouchRpcClient();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_client.Setup(new TouchSocketConfig()
+                .SetRemoteIPHost("127.0.0.1:7789")
+                .SetVerifyToken("TouchRpc")
+                .ConfigureContainer(a =>
+                {
+                    a.SetSingletonLogger(new EasyLogger(msg =>
+                    {
+                        this.listBox1.Items.Insert(0, msg);
+                    }));
+                }));
+            m_client.Connect();
+
+            m_client.Logger.Info("成功连接");
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.textBox1.Text.IsNullOrEmpty())
+                {
+                    this.m_client.Logger.Warning("路径不可为空。");
+                    return;
+                }
+                Result result = await this.m_client?.CreateDirectoryAsync(this.textBox1.Text);
+                this.m_client.Logger.Info(result.ToString());
+            }
+            catch (Exception ex)
+            {
+                this.m_client?.Logger.Exception(ex);
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.textBox1.Text.IsNullOrEmpty())
+                {
+                    this.m_client.Logger.Warning("路径不可为空。");
+                    return;
+                }
+                Result result = await this.m_client?.DeleteDirectoryAsync(this.textBox1.Text);
+                this.m_client.Logger.Info(result.ToString());
+            }
+            catch (Exception ex)
+            {
+                this.m_client?.Logger.Exception(ex);
+            }
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.textBox1.Text.IsNullOrEmpty())
+                {
+                    this.m_client.Logger.Warning("路径不可为空。");
+                    return;
+                }
+                RemoteDirectoryInfoResult result = await this.m_client?.GetDirectoryInfoAsync(this.textBox1.Text);
+                this.m_client.Logger.Info($"结果：{result.ResultCode}，信息：{result.Message}，更多信息请调试获得。");
+            }
+            catch (Exception ex)
+            {
+                this.m_client?.Logger.Exception(ex);
+            }
+        }
+
+        private async void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.textBox1.Text.IsNullOrEmpty())
+                {
+                    this.m_client.Logger.Warning("路径不可为空。");
+                    return;
+                }
+                Result result = await this.m_client?.DeleteFileAsync(this.textBox1.Text);
+                this.m_client.Logger.Info(result.ToString());
+            }
+            catch (Exception ex)
+            {
+                this.m_client?.Logger.Exception(ex);
+            }
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.textBox1.Text.IsNullOrEmpty())
+                {
+                    this.m_client.Logger.Warning("路径不可为空。");
+                    return;
+                }
+                RemoteFileInfoResult result = await this.m_client?.GetFileInfoAsync(this.textBox1.Text);
+                this.m_client.Logger.Info($"结果：{result.ResultCode}，信息：{result.Message}更多信息请调试获得。");
+            }
+            catch (Exception ex)
+            {
+                this.m_client?.Logger.Exception(ex);
+            }
+        }
     }
 }
--- examples/远程文件系统访问简单示例/ServerConsoleApp/Program.cs
+++ examples/远程文件系统访问简单示例/ServerConsoleApp/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using TouchSocket.Core;
 using TouchSocket.Core.Config;
 using TouchSocket.Core.Dependency;
 using TouchSocket.Core.Log;
+using TouchSocket.Core.Plugins;
 using TouchSocket.Rpc.TouchRpc;
+using TouchSocket.Rpc.TouchRpc.Plugins;
 using TouchSocket.Sockets;
 
 namespace ServerConsoleApp
@@ -11,9 +14,23 @@ namespace ServerConsoleApp
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Enterprise.ForTest();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
             var service = new TcpTouchRpcService();
             TouchSocketConfig config = new TouchSocketConfig()//配置
                    .SetListenIPHosts(new IPHost[] { new IPHost(7789) })
+                   .UsePlugin()
+                   .ConfigurePlugins(a =>
+                   {
+                       a.Add<MyPlugin>();
+                   })
                    .ConfigureContainer(a =>
                    {
                        a.SetLogger<LoggerGroup<ConsoleLogger, FileLogger>>();//注册一个日志组
@@ -24,6 +41,23 @@ namespace ServerConsoleApp
                 .Start();
 
             service.Logger.Info($"{service.GetType().Name}已启动，监听端口：{7789}");
+            Console.ReadKey();
+        }
+    }
+
+    class MyPlugin : TouchRpcPluginBase<TcpTouchRpcSocketClient>
+    {
+        protected override void OnRemoteAccessing(TcpTouchRpcSocketClient client, RemoteAccessActionEventArgs e)
+        {
+            client.Logger.Info($"用户{client.GetInfo()}正在进行文件系统访问，对象：{e.AccessType}，操作：{e.AccessMode}");
+            if (e.AccessMode== RemoteAccessMode.Delete)
+            {
+                e.IsPermitOperation = false;
+                e.Handled = true;
+                e.Message = "不允许删除文件";
+                return;
+            }
+            base.OnRemoteAccessing(client, e);
         }
     }
 }
