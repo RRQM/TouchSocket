d7fc8d44d80cbd5cb5c07c8638194a0d6f390e12
若汝棋茗 <505554090@qq.com>
2022-09-23 23:09:37 +0800

    更新引用

--- examples/BlogsDemos/LimitNumberOfConnectionsConsoleApp/Program.cs
+++ examples/BlogsDemos/LimitNumberOfConnectionsConsoleApp/Program.cs
@@ -11,6 +11,74 @@ using TouchSocket.Sockets.Plugins;
 
 namespace LimitNumberOfConnectionsConsoleApp
 {
+    internal class Count
+    {
+        private int num;
+
+        public int Num
+        {
+            get { return num; }
+        }
+
+        public int Decrement()
+        {
+            return Interlocked.Decrement(ref num);
+        }
+
+        public int Increment()
+        {
+            return Interlocked.Increment(ref num);
+        }
+    }
+
+    internal class LimitNumberOfConnectionsPlugin : TcpPluginBase
+    {
+        private readonly ConcurrentDictionary<string, Count> m_ipToCount = new ConcurrentDictionary<string, Count>();
+
+        private readonly ILog m_logger;
+
+        [DependencyInject(2)]
+        public LimitNumberOfConnectionsPlugin(int max, ILog logger)
+        {
+            this.Max = max;
+            this.m_logger = logger;
+
+            logger.Info($"限制连接插件生效，同一IP限制{max}个连接");
+        }
+        public LimitNumberOfConnectionsPlugin(ILog logger)
+        {
+            this.m_logger = logger;
+        }
+
+        public int Max { get; }
+        protected override void OnConnecting(ITcpClientBase client, ClientOperationEventArgs e)
+        {
+            Count count = m_ipToCount.GetOrAdd(client.IP, (s) => { return new Count(); });
+
+            if (count.Increment() > this.Max)
+            {
+                count.Decrement();
+                e.IsPermitOperation = false;//表示不许连接
+                e.Handled = true;//并且已经处理该消息。
+                this.m_logger.Warning($"IP={client.IP}的客户端，连接数达到设置阈值。已拒绝连接。");
+                return;
+            }
+            base.OnConnecting(client, e);
+        }
+
+        protected override void OnDisconnected(ITcpClientBase client, ClientDisconnectedEventArgs e)
+        {
+            if (m_ipToCount.TryGetValue(client.IP, out Count count))
+            {
+                if (count.Decrement() == 0)
+                {
+                    m_ipToCount.TryRemove(client.IP, out _);
+                }
+            }
+            base.OnDisconnected(client, e);
+        }
+    }
+
     internal class Program
     {
         /// <summary>
@@ -43,69 +111,6 @@ namespace LimitNumberOfConnectionsConsoleApp
             service.AddPlugin<LimitNumberOfConnectionsPlugin>();
 
             Console.ReadKey();
-        }
-    }
-
-    internal class LimitNumberOfConnectionsPlugin : TcpPluginBase
-    {
-        [DependencyInject(2)]
-        public LimitNumberOfConnectionsPlugin(int max, ILog logger)
-        {
-            this.Max = max;
-            this.Logger = logger;
-
-            logger.Info($"限制连接插件生效，同一IP限制{max}个连接");
-        }
-
-        private readonly ConcurrentDictionary<string, Count> m_ipToCount = new ConcurrentDictionary<string, Count>();
-
-        public int Max { get; }
-
-        protected override void OnConnecting(ITcpClientBase client, ClientOperationEventArgs e)
-        {
-            Count count = m_ipToCount.GetOrAdd(client.IP, (s) => { return new Count(); });
-
-            if (count.Increment() > this.Max)
-            {
-                count.Decrement();
-                e.IsPermitOperation = false;//表示不许连接
-                e.Handled = true;//并且已经处理该消息。
-                this.Logger.Warning($"IP={client.IP}的客户端，连接数达到设置阈值。已拒绝连接。");
-                return;
-            }
-            base.OnConnecting(client, e);
-        }
-
-        protected override void OnDisconnected(ITcpClientBase client, ClientDisconnectedEventArgs e)
-        {
-            if (m_ipToCount.TryGetValue(client.IP, out Count count))
-            {
-                if (count.Decrement() == 0)
-                {
-                    m_ipToCount.TryRemove(client.IP, out _);
-                }
-            }
-            base.OnDisconnected(client, e);
-        }
-    }
-
-    internal class Count
-    {
-        private int num;
-
-        public int Num
-        {
-            get { return num; }
-        }
-
-        public int Increment()
-        {
-            return Interlocked.Increment(ref num);
-        }
-
-        public int Decrement()
-        {
-            return Interlocked.Decrement(ref num);
         }
     }
 }
