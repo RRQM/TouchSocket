827a8b0e6070d949e384a7e49e19768846e618b4
若汝棋茗 <505554090@qq.com>
2022-09-26 15:04:13 +0800

    更新插件示例

--- examples/Tcp简单示例/AspNetCore/TcpServiceForWebApi/Plugins/MyTcpPlugin.cs
+++ examples/Tcp简单示例/AspNetCore/TcpServiceForWebApi/Plugins/MyTcpPlugin.cs
@@ -12,16 +12,16 @@ namespace TcpServiceForWebApi.Plugins
 {
     public class MyTcpPlugin:TcpPluginBase<SocketClient>
     {
-        private readonly ILog m_logger;
+        private ILogger<MyTcpPlugin> m_logger;
 
-        public MyTcpPlugin(ILog logger)
+        public MyTcpPlugin(ILogger<MyTcpPlugin> logger)
         {
             this.m_logger = logger;
         }
 
         protected override void OnConnected(SocketClient client, TouchSocketEventArgs e)
         {
-            m_logger.Info("客户端连接");
+            m_logger.LogInformation("客户端连接");
             base.OnConnected(client, e);
         }
     }
