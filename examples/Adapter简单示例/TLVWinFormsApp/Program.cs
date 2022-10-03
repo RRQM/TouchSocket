b3b45fae5fd70187ef91e3a53a2e5bb27762513f
若汝棋茗 <505554090@qq.com>
2022-10-03 21:54:50 +0800

    增加TLV示例

---
+++ examples/Adapter简单示例/TLVWinFormsApp/Program.cs
@@ -0,0 +1,26 @@
+using TouchSocket.Core;
+
+namespace TLVWinFormsApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                Enterprise.ForTest();
+            }
+            catch
+            {
+            }
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
