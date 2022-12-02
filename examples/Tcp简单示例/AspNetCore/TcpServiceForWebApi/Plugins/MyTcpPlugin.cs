ca9d75bfc69845c71c2805f089bd7927c8ad854e
若汝棋茗 <505554090@qq.com>
2022-12-02 12:55:22 +0800

    更新asp tcp示例

--- examples/Tcp简单示例/AspNetCore/TcpServiceForWebApi/Plugins/MyTcpPlugin.cs
+++ examples/Tcp简单示例/AspNetCore/TcpServiceForWebApi/Plugins/MyTcpPlugin.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using TouchSocket.Core;
+using TouchSocket.Core.ByteManager;
 using TouchSocket.Sockets;
 using TouchSocket.Sockets.Plugins;
 
@@ -18,6 +19,14 @@ namespace TcpServiceForWebApi.Plugins
         {
             m_logger.LogInformation("客户端连接");
             base.OnConnected(client, e);
+        }
+
+        protected override void OnReceivedData(SocketClient client, ReceivedDataEventArgs e)
+        {
+            //这里处理数据接收
+            //根据适配器类型，e.ByteBlock与e.RequestInfo会呈现不同的值，具体看文档=》适配器部分。
+            ByteBlock byteBlock = e.ByteBlock;
+            IRequestInfo requestInfo = e.RequestInfo;
         }
     }
 }
