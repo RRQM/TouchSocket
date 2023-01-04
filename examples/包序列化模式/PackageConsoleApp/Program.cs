8d82fa2f639902264e217c2dcde20964ed884f22
若汝棋茗 <505554090@qq.com>
2023-01-04 17:29:17 +0800

    T

---
+++ examples/包序列化模式/PackageConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using TouchSocket.Core;
+
+namespace PackageConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class MyClass : IPackage
+    {
+        public void Package(ByteBlock byteBlock)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unpackage(ByteBlock byteBlock)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
