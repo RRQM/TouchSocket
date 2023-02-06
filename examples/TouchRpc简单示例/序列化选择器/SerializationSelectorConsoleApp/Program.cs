bb389767caa1bb9daf037106073104678651a286
若汝棋茗 <505554090@qq.com>
2023-02-06 20:44:04 +0800

    增加序列化器示例

---
+++ examples/TouchRpc简单示例/序列化选择器/SerializationSelectorConsoleApp/Program.cs
@@ -0,0 +1,92 @@
+﻿using SerializationSelectorClassLibrary;
+using System.ComponentModel;
+using System.Threading.Channels;
+using TouchSocket.Core;
+using TouchSocket.Rpc;
+using TouchSocket.Rpc.TouchRpc;
+using TouchSocket.Sockets;
+
+namespace SerializationSelectorConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Enterprise.ForTest();//下列代码会用到源代码生成，所以启用企业版支持。
+            }
+            catch
+            {
+
+            }
+
+            StartServer();
+
+            var client = CreateClient();
+
+            var msg = client.Login(new LoginModel() { Account = "Account", Password = "Password" });
+            Console.WriteLine(msg);
+            Console.ReadKey();
+        }
+
+        static TcpTouchRpcClient CreateClient()
+        {
+            TcpTouchRpcClient client = new TcpTouchRpcClient();
+            client.Setup(new TouchSocketConfig()
+                .SetRemoteIPHost("127.0.0.1:7789")
+                .SetVerifyToken("TouchRpc"));
+            client.Connect();
+            return client;
+        }
+
+        static void StartServer()
+        {
+            var service = new TcpTouchRpcService();
+            var config = new TouchSocketConfig()//配置
+                   .SetListenIPHosts(new IPHost[] { new IPHost(7789) })
+                   .ConfigureContainer(a =>
+                   {
+                       a.AddConsoleLogger();
+                   })
+                   .ConfigureRpcStore(a =>
+                   {
+                       a.RegisterServer<MyRpcServer>();
+                   })
+                   .SetVerifyToken("TouchRpc");
+
+            service.Setup(config)
+                .Start();
+
+            service.Logger.Info($"{service.GetType().Name}已启动");
+        }
+    }
+
+    public class MyRpcServer : RpcServer
+    {
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns></returns>
+        [Description("登录")]
+        [TouchRpc]
+        public string Login(LoginModel loginModel)
+        {
+            return $"{loginModel.Account}-{loginModel.Password}";
+        }
+    }
+
+    [GeneratorRpcProxy("SerializationSelectorConsoleApp.MyRpcServer")]
+    public interface IMyRpcServer
+    {
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns></returns>
+        [Description("登录")]
+        [GeneratorRpcMethod]
+        string Login(LoginModel loginModel);
+    }
+}
