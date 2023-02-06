7a439dd0da61933bc94203aced0837355fcb11a9
若汝棋茗 <505554090@qq.com>
2023-02-06 23:16:27 +0800

    更新文档

--- handbook/sidebars.js
+++ handbook/sidebars.js
@@ -120,168 +120,168 @@ module.exports = {
     },
     {
       type: "category",
-      label: "04、Core",
+      label: "05、Core",
       items:
         [
           {
             type: "doc",
             id: "bytepool",
-            label: "4.1 内存池"
+            label: "5.1 内存池"
           },
           {
             type: "doc",
             id: "consoleaction",
-            label: "4.2 控制台行为"
+            label: "5.2 控制台行为"
           },
           {
             type: "doc",
             id: "touchsocketbitconverter",
-            label: "4.3 大小端转换器"
+            label: "5.3 大小端转换器"
           },
           {
             type: "doc",
             id: "datasecurity",
-            label: "4.4 数据加密"
+            label: "5.4 数据加密"
           },
           {
             type: "doc",
             id: "ilog",
-            label: "4.5 日志记录器"
+            label: "5.5 日志记录器"
           },
           {
             type: "doc",
             id: "appmessenger",
-            label: "4.6 应用信使"
+            label: "5.6 应用信使"
           },
           {
             type: "doc",
             id: "fastbinaryformatter",
-            label: "4.7 高性能二进制序列化"
+            label: "5.7 高性能二进制序列化"
           },
           {
             type: "doc",
             id: "jsonserialize",
-            label: "4.8 Json序列化"
+            label: "5.8 Json序列化"
           },
           {
             type: "doc",
             id: "ioc",
-            label: "4.9 依赖注入容器"
+            label: "5.9 依赖注入容器"
           },
           {
             type: "doc",
             id: "dependencyproperty",
-            label: "4.10 依赖属性"
+            label: "5.10 依赖属性"
           },
           {
             type: "doc",
             id: "filepool",
-            label: "4.11 文件流池"
+            label: "5.11 文件流池"
           },
           {
             type: "doc",
             id: "pluginsmanager",
-            label: "4.12 插件系统"
+            label: "5.12 插件系统"
           },
           {
             type: "doc",
             id: "ipackage",
-            label: "4.13 包序列化模式"
+            label: "5.13 包序列化模式"
           },
           {
             type: "doc",
             id: "othercore",
-            label: "4.14 其他相关功能类"
+            label: "5.14 其他相关功能类"
           },
         ]
     },
     {
       type: "category",
-      label: "05、Tcp组件",
+      label: "06、Tcp组件",
       items:
         [
           {
             type: "doc",
             id: "createtcpservice",
-            label: "5.1 创建TcpService"
+            label: "6.1 创建TcpService"
           },
           {
             type: "doc",
             id: "createtcpclient",
-            label: "5.2 创建TcpClient"
+            label: "6.2 创建TcpClient"
           },
           {
             type: "doc",
             id: "waitingclient",
-            label: "5.3 同步请求"
+            label: "6.3 同步请求"
           },
           {
             type: "doc",
             id: "natservice",
-            label: "5.4 Tcp端口转发"
+            label: "6.4 Tcp端口转发"
           },
           {
             type: "doc",
             id: "resetid",
-            label: "5.5 服务器重置ID"
+            label: "6.5 服务器重置ID"
           },
           {
             type: "doc",
             id: "reconnection",
-            label: "5.6 断线重连"
+            label: "6.6 断线重连"
           },
           {
             type: "doc",
             id: "tcpcommandlineplugin",
-            label: "5.7 命令行执行插件"
+            label: "6.7 命令行执行插件"
           },
           {
             type: "doc",
             id: "heartbeat",
-            label: "5.8 心跳设计"
+            label: "6.8 心跳设计"
           },
           {
             type: "doc",
             id: "tcpother",
-            label: "5.9 其他场景应用"
+            label: "6.9 其他场景应用"
           },
         ]
     },
     {
       type: "category",
-      label: "06、Udp组件",
+      label: "07、Udp组件",
       items:
         [
           {
             type: "doc",
             id: "createudpsession",
-            label: "6.1 创建UdpSession"
+            label: "7.1 创建UdpSession"
           },
           {
             type: "doc",
             id: "udptransmitbigdata",
-            label: "6.2 传输大于64K的数据"
+            label: "7.2 传输大于64K的数据"
           },
           {
             type: "doc",
             id: "udpbroadcast",
-            label: "6.3 组播、广播"
+            label: "7.3 组播、广播"
           },
         ]
     },
     {
       type: "category",
-      label: "07、数据处理适配器",
+      label: "08、数据处理适配器",
       items:
         [
           {
             type: "doc",
             id: "adapterdescription",
-            label: "7.1 介绍及使用"
+            label: "8.1 介绍及使用"
           },
           {
             type: "category",
-            label: "7.2 Tcp适配器",
+            label: "8.2 Tcp适配器",
             items:
               [
                 {
@@ -348,7 +348,7 @@ module.exports = {
           },
           {
             type: "category",
-            label: "7.3 Udp适配器",
+            label: "8.3 Udp适配器",
             items:
               [
                 {
@@ -360,7 +360,7 @@ module.exports = {
           },
           {
             type: "category",
-            label: "7.4 适配器案例赏析",
+            label: "8.4 适配器案例赏析",
             items:
               [
                 {
@@ -378,102 +378,102 @@ module.exports = {
           {
             type: "doc",
             id: "independentusedatahandlingadapter",
-            label: "7.5 独立使用适配器"
+            label: "8.5 独立使用适配器"
           },
           {
             type: "doc",
             id: "dataadaptertester",
-            label: "7.6 适配器完整性、性能测试"
+            label: "8.6 适配器完整性、性能测试"
           },
         ]
     },
     {
       type: "category",
-      label: "08、Http组件",
+      label: "09、Http组件",
       items:
         [
           {
             type: "doc",
             id: "createhttpservice",
-            label: "8.1 创建HttpService"
+            label: "9.1 创建HttpService"
           },
           {
             type: "doc",
             id: "createhttpclient",
-            label: "8.2 创建HttpClient"
+            label: "9.2 创建HttpClient"
           },
           {
             type: "doc",
             id: "httpstaticpageplugin",
-            label: "8.3 静态页面插件"
+            label: "9.3 静态页面插件"
           },
           {
             type: "doc",
             id: "httpfiletransfer",
-            label: "8.4 文件传输"
+            label: "9.4 文件传输"
           },
         ]
     },
     {
       type: "category",
-      label: "09、WebSocket组件",
+      label: "10、WebSocket组件",
       items:
         [
           {
             type: "doc",
             id: "websocketdescription",
-            label: "9.1 产品及架构介绍"
+            label: "10.1 产品及架构介绍"
           },
           {
             type: "doc",
             id: "createwebsocketservice",
-            label: "9.2 创建WebSocket服务器"
+            label: "10.2 创建WebSocket服务器"
           },
           {
             type: "doc",
             id: "createwebsocketclient",
-            label: "9.3 创建WebSocket客户端"
+            label: "10.3 创建WebSocket客户端"
           },
           {
             type: "doc",
             id: "wscommandlineplugin",
-            label: "9.4 WSCommandLinePlugin"
+            label: "10.4 WSCommandLinePlugin"
           },
           {
             type: "doc",
             id: "wsjsonrpc",
-            label: "9.5 基于WS的JsonRpc"
+            label: "10.5 基于WS的JsonRpc"
           },
         ]
     },
     {
       type: "category",
-      label: "10、TouchRpc组件",
+      label: "11、TouchRpc组件",
       items:
         [
           {
             type: "doc",
             id: "touchrpcdescription",
-            label: "10.1 产品及架构介绍"
+            label: "11.1 产品及架构介绍"
           },
           {
             type: "doc",
             id: "createtouchrpcservice",
-            label: "10.2 创建TouchRpc服务器"
+            label: "11.2 创建TouchRpc服务器"
           },
           {
             type: "doc",
             id: "createtouchrpcclient",
-            label: "10.3 创建TouchRpc客户端"
+            label: "11.3 创建TouchRpc客户端"
           },
           {
             type: "doc",
             id: "touchrpcbase",
-            label: "10.4 基础功能"
+            label: "11.4 基础功能"
           },
           {
             type: "category",
-            label: "10.5 Rpc功能",
+            label: "11.5 Rpc功能",
             items:
               [
                 {
@@ -498,14 +498,24 @@ module.exports = {
                 },
                 {
                   type: "doc",
+                  id: "rpcallcontext",
+                  label: "e.调用上下文"
+                },
+                {
+                  type: "doc",
+                  id: "rpcactionfilter",
+                  label: "f.Rpc服务AOP"
+                },
+                {
+                  type: "doc",
                   id: "generateproxy",
-                  label: "e.生成、获取代理"
+                  label: "f.生成、获取代理"
                 }
               ]
           },
           {
             type: "category",
-            label: "10.6 文件传输",
+            label: "11.6 文件传输",
             items:
               [
                 {
@@ -528,88 +538,88 @@ module.exports = {
           {
             type: "doc",
             id: "remotefilecontrol",
-            label: "10.7 远程文件操作"
+            label: "11.7 远程文件操作"
           },
           {
             type: "doc",
             id: "streamtransfer",
-            label: "10.8 Stream传输"
+            label: "11.8 Stream传输"
           },
           {
             type: "doc",
             id: "remotestreamaccess",
-            label: "10.9 远程流访问"
+            label: "11.9 远程流访问"
           },
           {
             type: "doc",
             id: "eventbus",
-            label: "10.10 EventBus"
+            label: "11.10 EventBus"
           }
         ]
     },
     {
       type: "category",
-      label: "11、WebApi组件",
+      label: "12、WebApi组件",
       items:
         [
           {
             type: "doc",
             id: "webapidescription",
-            label: "11.1 产品及架构介绍"
-          },
-          {
-            type: "doc",
-            id: "webapiservice",
-            label: "11.2 定义、发布、启动服务"
-          },
-          {
-            type: "doc",
-            id: "callwebapi",
-            label: "11.3 发现、调用服务"
-          },
-        ]
-    },
-    {
-      type: "category",
-      label: "12、JsonRpc组件",
-      items:
-        [
-          {
-            type: "doc",
-            id: "jsonrpcdescription",
             label: "12.1 产品及架构介绍"
           },
           {
             type: "doc",
-            id: "jsonrpcservice",
+            id: "webapiservice",
             label: "12.2 定义、发布、启动服务"
           },
           {
             type: "doc",
-            id: "calljsonrpc",
+            id: "callwebapi",
             label: "12.3 发现、调用服务"
           },
         ]
     },
     {
       type: "category",
-      label: "13、XmlRpc组件件",
+      label: "13、JsonRpc组件",
+      items:
+        [
+          {
+            type: "doc",
+            id: "jsonrpcdescription",
+            label: "13.1 产品及架构介绍"
+          },
+          {
+            type: "doc",
+            id: "jsonrpcservice",
+            label: "13.2 定义、发布、启动服务"
+          },
+          {
+            type: "doc",
+            id: "calljsonrpc",
+            label: "13.3 发现、调用服务"
+          },
+        ]
+    },
+    {
+      type: "category",
+      label: "14、XmlRpc组件件",
       items:
         [
           {
             type: "doc",
             id: "xmlrpcdescription",
-            label: "13.1 产品及架构介绍"
+            label: "14.1 产品及架构介绍"
           },
           {
             type: "doc",
             id: "xmlrpcservice",
-            label: "13.2 定义、发布、启动服务"
+            label: "14.2 定义、发布、启动服务"
           },
           {
             type: "doc",
             id: "callxmlrpc",
-            label: "13.3 发现、调用服务"
+            label: "14.3 发现、调用服务"
           },
         ]
     }
