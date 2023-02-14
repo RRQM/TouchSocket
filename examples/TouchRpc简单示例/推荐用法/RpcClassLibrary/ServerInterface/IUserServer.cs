8c34dadeb7b444ab5f2acd948a429c997b87fa01
若汝棋茗 <505554090@qq.com>
2023-02-14 21:54:01 +0800

    更新demo

--- examples/TouchRpc简单示例/推荐用法/RpcClassLibrary/ServerInterface/IUserServer.cs
+++ examples/TouchRpc简单示例/推荐用法/RpcClassLibrary/ServerInterface/IUserServer.cs
@@ -10,11 +10,11 @@ namespace RpcClassLibrary.ServerInterface
     /// <summary>
     /// 定义服务接口。
     /// </summary>
-    [GeneratorRpcProxy(Prefix = "RpcClassLibrary")]
+    [GeneratorRpcProxy(Prefix = "RpcClassLibrary",MethodFlags = MethodFlags.IncludeCallContext)]
     public interface IUserServer:IRpcServer
     {
         [GeneratorRpcMethod]
         [TouchRpc]
-        LoginResponse Login(LoginRequest request);
+        LoginResponse Login(ICallContext callContext,LoginRequest request);
     }
 }
--- examples/TouchRpc简单示例/推荐用法/RpcImplementationClassLibrary/UserServer.cs
+++ examples/TouchRpc简单示例/推荐用法/RpcImplementationClassLibrary/UserServer.cs
@@ -2,15 +2,16 @@
 using RpcClassLibrary.ServerInterface;
 using System;
 using TouchSocket.Core;
+using TouchSocket.Rpc;
 
 namespace RpcImplementationClassLibrary
 {
     public class UserServer : IUserServer
     {
-        public LoginResponse Login(LoginRequest request)
+        public LoginResponse Login(ICallContext callContext, LoginRequest request)
         {
             //返回假逻辑
-            return new LoginResponse() {  Result=Result.Success};
+            return new LoginResponse() { Result = Result.Success };
         }
     }
 }
