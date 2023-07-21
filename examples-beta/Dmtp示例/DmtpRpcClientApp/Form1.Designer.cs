bd6a25d7a653df3c337583af66c6af0caaf00282
若汝棋茗 <505554090@qq.com>
2023-07-21 22:44:48 +0800

    DmtpRpcClientApp

---
+++ examples-beta/Dmtp示例/DmtpRpcClientApp/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace TouchRpcClientApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 71);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(268, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "直接调用Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += this.button1_Click;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 93);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "账号：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 88);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 38);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "123";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(454, 88);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(196, 38);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "abc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(372, 93);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "密码：";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 124);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(268, 42);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "代理调用Login";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += this.button2_Click;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 177);
+            this.button3.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(268, 42);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "扩展调用Login";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += this.button3_Click;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(693, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.groupBox1.Size = new System.Drawing.Size(292, 239);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tcp调用";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1192, 294);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += this.Form1_Load;
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ examples-beta/Dmtp示例/DmtpRpcClientApp/Form1.cs
@@ -0,0 +1,59 @@
+﻿using RpcProxy;
+using System;
+using System.Windows.Forms;
+using TouchSocket.Core;
+using TouchSocket.Dmtp;
+using TouchSocket.Dmtp.Rpc;
+using TouchSocket.Rpc;
+using TouchSocket.Sockets;
+
+namespace TouchRpcClientApp
+{
+    public partial class Form1 : Form
+    {
+        private TcpDmtpClient client = new TcpDmtpClient();
+
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //直接调用时，第一个参数为调用键，服务类全名+方法名（必须全小写）
+            //第二个参数为调用配置参数，可设置调用超时时间，取消调用等功能。
+            //后续参数为调用参数。
+            var result = this.client.GetDmtpRpcActor().InvokeT<bool>("Login", InvokeOption.WaitInvoke, this.textBox1.Text, this.textBox2.Text);
+            MessageBox.Show(result.ToString());
+            this.client.SafeDispose();//client是长连接，可以复用，但在此处使用短连接。
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var myRpcServer = new RpcProxy.MyRpcServer(this.client.GetDmtpRpcActor());//MyRpcServer类是由代码工具生成的类。
+
+            //代理调用时，基本和本地调用一样。只是会多一个调用配置参数。
+            var result = myRpcServer.Login(this.textBox1.Text, this.textBox2.Text, InvokeOption.WaitInvoke);
+            MessageBox.Show(result.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //扩展调用时，首先要保证本地已有代理文件，然后调用和和本地调用一样。只是会多一个调用配置参数。
+            var result = this.client.GetDmtpRpcActor().Login(this.textBox1.Text, this.textBox2.Text, InvokeOption.WaitInvoke);
+            MessageBox.Show(result.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.client.Setup(new TouchSocketConfig()
+                .SetRemoteIPHost("127.0.0.1:7789")
+                .ConfigurePlugins(a =>
+                {
+                    a.UseDmtpRpc();
+                })
+                .SetVerifyToken("Rpc"));
+            this.client.Connect();
+        }
+    }
+}
