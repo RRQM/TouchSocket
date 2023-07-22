18e351699387a167a8842847b51efcb9807c2975
若汝棋茗 <505554090@qq.com>
2023-07-22 10:25:08 +0800

    DmtpWebApplication

---
+++ examples-beta/Dmtp示例/DmtpWebApplication/Program.cs
@@ -0,0 +1,79 @@
+using TouchSocket.Core;
+using TouchSocket.Dmtp;
+using TouchSocket.Dmtp.Rpc;
+using TouchSocket.Sockets;
+
+namespace DmtpWebApplication
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                Enterprise.ForTest();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddWebsocketDmtpService(() =>
+            {
+                return new TouchSocketConfig()
+                    .SetVerifyToken("Dmtp")
+                    .UseAspNetCoreContainer(builder.Services)
+                    .ConfigureContainer(a =>
+                    {
+                        a.AddDmtpRouteService();
+                    })
+                    .ConfigurePlugins(a =>
+                    {
+                        //添加插件
+                    });
+            });
+
+            //企业版功能
+            builder.Services.AddHttpMiddlewareDmtpService(() =>
+            {
+                return new TouchSocketConfig()
+                        .SetVerifyToken("Dmtp")
+                        .UseAspNetCoreContainer(builder.Services)
+                        .ConfigureContainer(a =>
+                        {
+                            a.AddDmtpRouteService();
+                        })
+                        .ConfigurePlugins(a =>
+                        {
+                            //添加插件
+                        });
+            });
+
+            var app = builder.Build();
+            app.UseWebSockets();
+            app.UseWebsocketDmtp("/WebsocketDmtp");//WebsocketDmtp必须在UseWebSockets之后使用。
+
+            app.UseHttpDmtp(); //HttpDmtp可以单独直接使用。
+
+            _ = app.RunAsync("http://localhost:5174");
+
+            //WebsocketDmtpClient连接
+            var websocketDmtpClient = new WebsocketDmtpClient();
+            websocketDmtpClient.Setup(new TouchSocketConfig()
+                .SetVerifyToken("Dmtp")
+                .SetRemoteIPHost("ws://localhost:5174/WebsocketDmtp"));
+            await websocketDmtpClient.ConnectAsync();
+            Console.WriteLine("WebsocketDmtpClient连接成功");
+
+            var httpDmtpClient = new HttpDmtpClient();
+            httpDmtpClient.Setup(new TouchSocketConfig()
+                .SetVerifyToken("Dmtp")
+                .SetRemoteIPHost("http://127.0.0.1:5174"));
+            httpDmtpClient.Connect();
+            Console.WriteLine("HttpDmtpClient连接成功");
+
+            Console.ReadKey();
+        }
+    }
+}
