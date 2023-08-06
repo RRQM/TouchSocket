6ddc39d65ca26b421aaf1419e1c0109e25e33eff
若汝棋茗 <505554090@qq.com>
2023-08-06 23:25:25 +0800

    更新文档，更新demo

--- examples-beta/Dmtp示例/FileTransferConsoleApp/Program.cs
+++ examples-beta/Dmtp示例/FileTransferConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using TouchSocket.Core;
 using TouchSocket.Dmtp;
 using TouchSocket.Dmtp.FileTransfer;
+using TouchSocket.Dmtp.Rpc;
 using TouchSocket.Sockets;
 
 namespace FileTransferConsoleApp
@@ -37,15 +38,212 @@ namespace FileTransferConsoleApp
             consoleAction.Add("5", "测试客户端向其他客户端请求文件", () => { ClientPullFileFromClient(); });
             consoleAction.Add("6", "测试客户端向其他客户端推送文件", () => { ClientPushFileFromClient(); });
 
+            consoleAction.Add("7", "测试客户端向服务器请求小文件", () => { PullSmallFileFromService(client); });
+            consoleAction.Add("8", "测试客户端向服务器推送小文件", () => { PushSmallFileFromService(client); });
+
+            consoleAction.Add("9", "测试客户端工厂向服务器请求文件", () => { MultithreadingClientPullFileFromService(); });
+            consoleAction.Add("10", "测试客户端工厂向服务器推送文件", () => { MultithreadingClientPushFileFromService(); });
+
             consoleAction.ShowAll();
 
-            while (true) 
+            while (true)
             {
                 if (!consoleAction.Run(Console.ReadLine()))
                 {
                     consoleAction.ShowAll();
                 }
             }
+        }
+
+        /// <summary>
+        /// 多线程推送文件
+        /// </summary>
+        static void MultithreadingClientPushFileFromService()
+        {
+            using var clientFactory = CreateClientFactory();
+            Result resultCon = clientFactory.CheckStatus();//检验连接状态，一般当主通行器连接时，即认为在连接状态。
+
+            if (!resultCon.IsSuccess())
+            {
+                //没有连接
+                return;
+            }
+            ConsoleLogger.Default.Info("开始向服务器推送文件");
+            var filePath = "MultithreadingClientPushFileFromService.Test";
+            var saveFilePath = "SaveMultithreadingClientPushFileFromService.Test";
+            if (!File.Exists(filePath))//创建服务器端的测试文件
+            {
+                using (var stream = File.OpenWrite(filePath))
+                {
+                    stream.SetLength(FileLength);
+                }
+            }
+
+            var metadata = new Metadata();//传递到服务器的元数据
+            metadata.Add("1", "1");
+            metadata.Add("2", "2");
+
+            var fileOperator = new MultithreadingFileOperator//实例化本次传输的控制器，用于获取传输进度、速度、状态等。
+            {
+                SavePath = saveFilePath,//客户端本地保存路径
+                ResourcePath = filePath,//请求文件的资源路径
+                Metadata = metadata,//传递到服务器的元数据
+                Timeout = TimeSpan.FromSeconds(60),//传输超时时长
+                TryCount = 10,//当遇到失败时，尝试次数
+                FileSectionSize = 1024 * 512,//分包大小，当网络较差时，应该适当减小该值
+                MultithreadingCount = 10//多线程数量
+            };
+
+            fileOperator.SetMaxSpeed(MaxSpeed);//设置最大限速为1Mb。
+
+            //此处的作用相当于Timer，定时每秒输出当前的传输进度和速度。
+            var loopAction = LoopAction.CreateLoopAction(-1, 1000, (loop) =>
+            {
+                if (fileOperator.Result.ResultCode != ResultCode.Default)
+                {
+                    loop.Dispose();
+                }
+                ConsoleLogger.Default.Info($"进度：{fileOperator.Progress}，速度：{fileOperator.Speed()}");
+            });
+
+            loopAction.RunAsync();
+
+            //此方法会阻塞，直到传输结束，也可以使用PushFileAsync
+            IResult result = clientFactory.PushFile(fileOperator);
+
+            ConsoleLogger.Default.Info($"向服务器推送文件结束，{result}");
+
+            File.Delete(filePath);
+            File.Delete(saveFilePath);
+        }
+
+        /// <summary>
+        /// 多线程请求文件
+        /// </summary>
+        static void MultithreadingClientPullFileFromService()
+        {
+            using var clientFactory = CreateClientFactory();
+            Result resultCon = clientFactory.CheckStatus();//检验连接状态，一般当主通行器连接时，即认为在连接状态。
+            if (!resultCon.IsSuccess())
+            {
+                //没有连接
+                return;
+            }
+            ConsoleLogger.Default.Info("开始从服务器下载文件");
+            var filePath = "MultithreadingClientPullFileFromService.Test";
+            var saveFilePath = "SaveMultithreadingClientPullFileFromService.Test";
+            if (!File.Exists(filePath))//创建服务器端的测试文件
+            {
+                using (var stream = File.OpenWrite(filePath))
+                {
+                    stream.SetLength(FileLength);
+                }
+            }
+
+            var metadata = new Metadata();//传递到服务器的元数据
+            metadata.Add("1", "1");
+            metadata.Add("2", "2");
+
+            var fileOperator = new MultithreadingFileOperator//实例化本次传输的控制器，用于获取传输进度、速度、状态等。
+            {
+                SavePath = saveFilePath,//客户端本地保存路径
+                ResourcePath = filePath,//请求文件的资源路径
+                Metadata = metadata,//传递到服务器的元数据
+                Timeout = TimeSpan.FromSeconds(60),//传输超时时长
+                TryCount = 10,//当遇到失败时，尝试次数
+                FileSectionSize = 1024 * 512,//分包大小，当网络较差时，应该适当减小该值
+                MultithreadingCount = 10//多线程数量
+            };
+
+            fileOperator.SetMaxSpeed(MaxSpeed);//设置最大限速为1Mb。
+
+            //此处的作用相当于Timer，定时每秒输出当前的传输进度和速度。
+            var loopAction = LoopAction.CreateLoopAction(-1, 1000, (loop) =>
+            {
+                if (fileOperator.Result.ResultCode != ResultCode.Default)
+                {
+                    loop.Dispose();
+                }
+                ConsoleLogger.Default.Info($"进度：{fileOperator.Progress}，速度：{fileOperator.Speed()}");
+            });
+
+            loopAction.RunAsync();
+
+            //此方法会阻塞，直到传输结束，也可以使用PullFileAsync
+            IResult result = clientFactory.PullFile(fileOperator);
+
+            ConsoleLogger.Default.Info($"从服务器下载文件结束，{result}");
+
+            File.Delete(filePath);
+            File.Delete(saveFilePath);
+        }
+
+        /// <summary>
+        /// 推送小文件
+        /// </summary>
+        /// <param name="client"></param>
+        static void PushSmallFileFromService(TcpDmtpClient client)
+        {
+            ConsoleLogger.Default.Info("开始从服务器下载小文件");
+            var filePath = "PushSmallFileFromService.Test";
+            var saveFilePath = "SavePushSmallFileFromService.Test";
+            if (!File.Exists(filePath))//创建测试文件
+            {
+                using (var stream = File.OpenWrite(filePath))
+                {
+                    stream.SetLength(1024);
+                }
+            }
+
+            var metadata = new Metadata();//传递到服务器的元数据
+            metadata.Add("1", "1");
+            metadata.Add("2", "2");
+
+            //此方法会阻塞，直到传输结束，也可以使用PullSmallFileAsync
+            var result = client.GetDmtpFileTransferActor().PushSmallFile(saveFilePath, new FileInfo(filePath), metadata);
+            if (result.IsSuccess())
+            {
+                //成功
+            }
+
+            ConsoleLogger.Default.Info($"从服务器下载小文件结束，结果：{result}");
+            client.Logger.Info(result.ToString());
+
+            File.Delete(filePath);
+            File.Delete(saveFilePath);
+        }
+
+        /// <summary>
+        /// 请求小文件
+        /// </summary>
+        /// <param name="client"></param>
+        static void PullSmallFileFromService(TcpDmtpClient client)
+        {
+            ConsoleLogger.Default.Info("开始从服务器下载小文件");
+            var filePath = "PullSmallFileFromService.Test";
+            var saveFilePath = "SavePullSmallFileFromService.Test";
+            if (!File.Exists(filePath))//创建测试文件
+            {
+                using (var stream = File.OpenWrite(filePath))
+                {
+                    stream.SetLength(1024);
+                }
+            }
+
+            var metadata = new Metadata();//传递到服务器的元数据
+            metadata.Add("1", "1");
+            metadata.Add("2", "2");
+
+            //此方法会阻塞，直到传输结束，也可以使用PullSmallFileAsync
+            PullSmallFileResult result = client.GetDmtpFileTransferActor().PullSmallFile(filePath, metadata);
+            byte[] data = result.Value;//此处即是下载的小文件的实际数据
+            result.Save(saveFilePath, overwrite: true);//将数据保存到指定路径。
+
+            ConsoleLogger.Default.Info("从服务器下载小文件结束");
+            client.Logger.Info(result.ToString());
+
+            File.Delete(filePath);
+            File.Delete(saveFilePath);
         }
 
         private static void ConsoleAction_OnException(Exception obj)
@@ -447,6 +645,52 @@ namespace FileTransferConsoleApp
                 .Start();
             service.Logger.Info("服务器成功启动");
             return service;
+        }
+
+        private static TcpDmtpClientFactory CreateClientFactory()
+        {
+            try
+            {
+                Enterprise.ForTest();
+            }
+            catch (Exception ex)
+            {
+                ConsoleLogger.Default.Exception(ex);
+            }
+            var clientFactory = new TcpDmtpClientFactory()
+            {
+                MinCount = 5,
+                MaxCount = 10,
+                OnGetTransferConfig = () => //配置辅助通信
+                {
+                    return new TouchSocketConfig()
+                    .SetRemoteIPHost("127.0.0.1:7789")
+                    .SetVerifyToken("File")
+                    .SetBufferLength(1024 * 1024)
+                    .ConfigurePlugins(a =>
+                    {
+                        a.UseDmtpFileTransfer();
+                    });
+                }
+            };
+            clientFactory.MainConfig//配置主通信
+                         .SetRemoteIPHost("127.0.0.1:7789")
+                         .SetVerifyToken("File")
+                         .SetBufferLength(1024 * 1024)
+                         .ConfigurePlugins(a =>
+                         {
+                             a.UseDmtpFileTransfer();
+                         });
+
+            clientFactory.OnFindTransferIds = (client, targetId) =>
+            {
+                //此处的操作不唯一，可能需要rpc实现。
+                //其目的比较简单，就是获取到targetId对应的主客户端的所有传输客户端的Id集合。
+                //这样就实现了多个客户端向多个客户端传输文件的目的。
+
+                return new string[] { targetId };//此处为模拟结果。
+            };
+            return clientFactory;
         }
     }
 
