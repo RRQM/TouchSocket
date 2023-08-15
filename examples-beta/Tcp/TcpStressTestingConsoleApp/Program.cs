af07ddaa7bc2ad2df2dce51fc61bee2a3605ae4d
若汝棋茗 <505554090@qq.com>
2023-08-15 21:34:24 +0800

    更新demo

--- examples-beta/Tcp/TcpStressTestingConsoleApp/Program.cs
+++ examples-beta/Tcp/TcpStressTestingConsoleApp/Program.cs
@@ -8,19 +8,24 @@ namespace TcpStressTestingConsoleApp
     {
         static void Main(string[] args)
         {
-            BytePool.SetDefault(new BytePool(1024*1024,100));
-            Console.WriteLine($"当前内存池容量：{BytePool.Default.Capacity/(1048576.0):0.00}Mb");
+            //BytePool.SetDefault(new BytePool(1024*1024,100));
+            Console.WriteLine($"当前内存池容量：{BytePool.Default.Capacity / (1048576.0):0.00}Mb");
             var service = GetTcpService();
 
-            for (int i = 0; i < 5; i++)
-            {
-                Process.Start(Path.GetFullPath("TcpStressTestingClientConsoleApp.exe"));
-            }
-            
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    ProcessStartInfo info = new ProcessStartInfo()
+            //    {
+            //        UseShellExecute = false,
+            //        FileName = Path.GetFullPath("TcpStressTestingClientConsoleApp.exe")
+            //    };
+            //    Process.Start(info);
+            //}
+
             Console.ReadKey();
         }
 
-       
+
 
         static TcpService GetTcpService()
         {
@@ -42,6 +47,7 @@ namespace TcpStressTestingConsoleApp
             service.Setup(new TouchSocketConfig()//载入配置
                                                  //.UseDelaySender()
                 .SetListenIPHosts("tcp://127.0.0.1:7789", 7790)//同时监听两个地址
+                .SetBufferLength(1024 * 2)
                 .ConfigureContainer(a =>//容器的配置顺序应该在最前面
                 {
                     a.AddConsoleLogger();//添加一个控制台日志注入（注意：在maui中控制台日志不可用）
