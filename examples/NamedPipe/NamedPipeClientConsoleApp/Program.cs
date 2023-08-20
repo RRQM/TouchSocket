bcdc781094ab5dd187773d7d7295c745f21d06f7
若汝棋茗 <505554090@qq.com>
2023-08-20 15:02:29 +0800

    新增命名管道压力测试demo

--- examples/NamedPipe/NamedPipeClientConsoleApp/Program.cs
+++ examples/NamedPipe/NamedPipeClientConsoleApp/Program.cs
@@ -19,18 +19,18 @@ namespace NamedPipeClientConsoleApp
 
         private static NamedPipeClient CreateClient()
         {
-            var tcpClient = new NamedPipeClient();
-            tcpClient.Connected = (client, e) => { };//成功连接到服务器
-            tcpClient.Disconnected = (client, e) => { };//从服务器断开连接，当连接不成功时不会触发。
-            tcpClient.Received = (client, byteBlock, requestInfo) =>
+            var client = new NamedPipeClient();
+            client.Connected = (client, e) => { };//成功连接到服务器
+            client.Disconnected = (client, e) => { };//从服务器断开连接，当连接不成功时不会触发。
+            client.Received = (client, byteBlock, requestInfo) =>
             {
                 //从服务器收到信息
                 var mes = Encoding.UTF8.GetString(byteBlock.Buffer, 0, byteBlock.Len);
-                tcpClient.Logger.Info($"客户端接收到信息：{mes}");
+                client.Logger.Info($"客户端接收到信息：{mes}");
             };
 
             //载入配置
-            tcpClient.Setup(new TouchSocketConfig()
+            client.Setup(new TouchSocketConfig()
                 .SetPipeServer(".")//一般本机管道时，可以不用此配置
                 .SetPipeName("TouchSocketPipe")//管道名称
                 .ConfigurePlugins(a =>
@@ -42,8 +42,8 @@ namespace NamedPipeClientConsoleApp
                     a.AddConsoleLogger();//添加一个日志注入
                 }))
                 .Connect();
-            tcpClient.Logger.Info("客户端成功连接");
-            return tcpClient;
+            client.Logger.Info("客户端成功连接");
+            return client;
         }
     }
 }
