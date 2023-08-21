63cc31ac3133036578489dddc19a73c16186525a
若汝棋茗 <505554090@qq.com>
2023-08-21 23:02:20 +0800

    更新demo，更新文档，新增Rpc泛型限制，Tcp心跳

--- examples/Dmtp/DmtpRpcServerConsoleApp/RpcProxy.cs
+++ examples/Dmtp/DmtpRpcServerConsoleApp/RpcProxy.cs
@@ -1,240 +1,220 @@
-using
-/* 项目“DmtpRpcClientConsoleApp”的未合并的更改
-在此之前:
+using System;
 using TouchSocket.Core;
 using TouchSocket.Sockets;
 using TouchSocket.Rpc;
-using TouchSocket.Dmtp.Rpc;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
-在此之后:
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
-using System.Threading.Tasks;
-using TouchSocket.Core;
-using TouchSocket.Dmtp.Rpc;
-using TouchSocket.Rpc;
-using TouchSocket.Sockets;
-*/
-TouchSocket.Rpc;
 namespace RpcProxy
 {
-    public interface IMyRpcServer : TouchSocket.Rpc.IRemoteServer
-    {
-        ///<summary>
-        ///将两个数相加
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        System.Int32 Add(System.Int32 a, System.Int32 b, IInvokeOption invokeOption = default);
-        ///<summary>
-        ///将两个数相加
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        Task<System.Int32> AddAsync(System.Int32 a, System.Int32 b, IInvokeOption invokeOption = default);
+public interface IMyRpcServer:TouchSocket.Rpc.IRemoteServer
+{
+///<summary>
+///将两个数相加
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+System.Int32 Add(System.Int32 a,System.Int32 b,IInvokeOption invokeOption = default);
+///<summary>
+///将两个数相加
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+Task<System.Int32> AddAsync(System.Int32 a,System.Int32 b,IInvokeOption invokeOption = default);
 
-        ///<summary>
-        ///测试客户端请求，服务器响应大量流数据
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        System.Int32 RpcPullChannel(System.Int32 channelID, IInvokeOption invokeOption = default);
-        ///<summary>
-        ///测试客户端请求，服务器响应大量流数据
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        Task<System.Int32> RpcPullChannelAsync(System.Int32 channelID, IInvokeOption invokeOption = default);
+///<summary>
+///测试客户端请求，服务器响应大量流数据
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+System.Int32 RpcPullChannel(System.Int32 channelID,IInvokeOption invokeOption = default);
+///<summary>
+///测试客户端请求，服务器响应大量流数据
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+Task<System.Int32> RpcPullChannelAsync(System.Int32 channelID,IInvokeOption invokeOption = default);
 
-        ///<summary>
-        ///测试客户端推送流数据
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        System.Int32 RpcPushChannel(System.Int32 channelID, IInvokeOption invokeOption = default);
-        ///<summary>
-        ///测试客户端推送流数据
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        Task<System.Int32> RpcPushChannelAsync(System.Int32 channelID, IInvokeOption invokeOption = default);
+///<summary>
+///测试客户端推送流数据
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+System.Int32 RpcPushChannel(System.Int32 channelID,IInvokeOption invokeOption = default);
+///<summary>
+///测试客户端推送流数据
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+Task<System.Int32> RpcPushChannelAsync(System.Int32 channelID,IInvokeOption invokeOption = default);
 
-    }
-    public class MyRpcServer : IMyRpcServer
-    {
-        public MyRpcServer(IRpcClient client)
-        {
-            this.Client = client;
-        }
-        public IRpcClient Client { get; private set; }
-        ///<summary>
-        ///将两个数相加
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        public System.Int32 Add(System.Int32 a, System.Int32 b, IInvokeOption invokeOption = default)
-        {
-            if (this.Client == null)
-            {
-                throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
-            }
-            var parameters = new object[] { a, b };
-            var returnData = (System.Int32)this.Client.Invoke(typeof(System.Int32), "Add", invokeOption, parameters);
-            return returnData;
-        }
-        ///<summary>
-        ///将两个数相加
-        ///</summary>
-        public async Task<System.Int32> AddAsync(System.Int32 a, System.Int32 b, IInvokeOption invokeOption = default)
-        {
-            if (this.Client == null)
-            {
-                throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
-            }
-            var parameters = new object[] { a, b };
-            return (System.Int32)await this.Client.InvokeAsync(typeof(System.Int32), "Add", invokeOption, parameters);
-        }
+}
+public class MyRpcServer :IMyRpcServer
+{
+public MyRpcServer(IRpcClient client)
+{
+this.Client=client;
+}
+public IRpcClient Client{get;private set; }
+///<summary>
+///将两个数相加
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+public System.Int32 Add(System.Int32 a,System.Int32 b,IInvokeOption invokeOption = default)
+{
+if(Client==null)
+{
+throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
+}
+object[] parameters = new object[]{a,b};
+System.Int32 returnData=(System.Int32)Client.Invoke(typeof(System.Int32),"Add",invokeOption, parameters);
+return returnData;
+}
+///<summary>
+///将两个数相加
+///</summary>
+public async Task<System.Int32> AddAsync(System.Int32 a,System.Int32 b,IInvokeOption invokeOption = default)
+{
+if(Client==null)
+{
+throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
+}
+object[] parameters = new object[]{a,b};
+return (System.Int32) await Client.InvokeAsync(typeof(System.Int32),"Add",invokeOption, parameters);
+}
 
-        ///<summary>
-        ///测试客户端请求，服务器响应大量流数据
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        public System.Int32 RpcPullChannel(System.Int32 channelID, IInvokeOption invokeOption = default)
-        {
-            if (this.Client == null)
-            {
-                throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
-            }
-            var parameters = new object[] { channelID };
-            var returnData = (System.Int32)this.Client.Invoke(typeof(System.Int32), "consoleapp2.program+myrpcserver.rpcpullchannel", invokeOption, parameters);
-            return returnData;
-        }
-        ///<summary>
-        ///测试客户端请求，服务器响应大量流数据
-        ///</summary>
-        public async Task<System.Int32> RpcPullChannelAsync(System.Int32 channelID, IInvokeOption invokeOption = default)
-        {
-            if (this.Client == null)
-            {
-                throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
-            }
-            var parameters = new object[] { channelID };
-            return (System.Int32)await this.Client.InvokeAsync(typeof(System.Int32), "consoleapp2.program+myrpcserver.rpcpullchannel", invokeOption, parameters);
-        }
+///<summary>
+///测试客户端请求，服务器响应大量流数据
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+public System.Int32 RpcPullChannel(System.Int32 channelID,IInvokeOption invokeOption = default)
+{
+if(Client==null)
+{
+throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
+}
+object[] parameters = new object[]{channelID};
+System.Int32 returnData=(System.Int32)Client.Invoke(typeof(System.Int32),"consoleapp2.program+myrpcserver.rpcpullchannel",invokeOption, parameters);
+return returnData;
+}
+///<summary>
+///测试客户端请求，服务器响应大量流数据
+///</summary>
+public async Task<System.Int32> RpcPullChannelAsync(System.Int32 channelID,IInvokeOption invokeOption = default)
+{
+if(Client==null)
+{
+throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
+}
+object[] parameters = new object[]{channelID};
+return (System.Int32) await Client.InvokeAsync(typeof(System.Int32),"consoleapp2.program+myrpcserver.rpcpullchannel",invokeOption, parameters);
+}
 
-        ///<summary>
-        ///测试客户端推送流数据
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        public System.Int32 RpcPushChannel(System.Int32 channelID, IInvokeOption invokeOption = default)
-        {
-            if (this.Client == null)
-            {
-                throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
-            }
-            var parameters = new object[] { channelID };
-            var returnData = (System.Int32)this.Client.Invoke(typeof(System.Int32), "consoleapp2.program+myrpcserver.rpcpushchannel", invokeOption, parameters);
-            return returnData;
-        }
-        ///<summary>
-        ///测试客户端推送流数据
-        ///</summary>
-        public async Task<System.Int32> RpcPushChannelAsync(System.Int32 channelID, IInvokeOption invokeOption = default)
-        {
-            if (this.Client == null)
-            {
-                throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
-            }
-            var parameters = new object[] { channelID };
-            return (System.Int32)await this.Client.InvokeAsync(typeof(System.Int32), "consoleapp2.program+myrpcserver.rpcpushchannel", invokeOption, parameters);
-        }
+///<summary>
+///测试客户端推送流数据
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+public System.Int32 RpcPushChannel(System.Int32 channelID,IInvokeOption invokeOption = default)
+{
+if(Client==null)
+{
+throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
+}
+object[] parameters = new object[]{channelID};
+System.Int32 returnData=(System.Int32)Client.Invoke(typeof(System.Int32),"consoleapp2.program+myrpcserver.rpcpushchannel",invokeOption, parameters);
+return returnData;
+}
+///<summary>
+///测试客户端推送流数据
+///</summary>
+public async Task<System.Int32> RpcPushChannelAsync(System.Int32 channelID,IInvokeOption invokeOption = default)
+{
+if(Client==null)
+{
+throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
+}
+object[] parameters = new object[]{channelID};
+return (System.Int32) await Client.InvokeAsync(typeof(System.Int32),"consoleapp2.program+myrpcserver.rpcpushchannel",invokeOption, parameters);
+}
 
-    }
-    public static class MyRpcServerExtensions
-    {
-        ///<summary>
-        ///将两个数相加
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        public static System.Int32 Add<TClient>(this TClient client, System.Int32 a, System.Int32 b, IInvokeOption invokeOption = default) where TClient :
-        TouchSocket.Rpc.IRpcClient
-        {
-            var parameters = new object[] { a, b };
-            var returnData = (System.Int32)client.Invoke(typeof(System.Int32), "Add", invokeOption, parameters);
-            return returnData;
-        }
-        ///<summary>
-        ///将两个数相加
-        ///</summary>
-        public static async Task<System.Int32> AddAsync<TClient>(this TClient client, System.Int32 a, System.Int32 b, IInvokeOption invokeOption = default) where TClient :
-        TouchSocket.Rpc.IRpcClient
-        {
-            var parameters = new object[] { a, b };
-            return (System.Int32)await client.InvokeAsync(typeof(System.Int32), "Add", invokeOption, parameters);
-        }
+}
+public static class MyRpcServerExtensions
+{
+///<summary>
+///将两个数相加
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+public static System.Int32 Add<TClient>(this TClient client,System.Int32 a,System.Int32 b,IInvokeOption invokeOption = default) where TClient:
+TouchSocket.Rpc.IRpcClient{
+object[] parameters = new object[]{a,b};
+System.Int32 returnData=(System.Int32)client.Invoke(typeof(System.Int32),"Add",invokeOption, parameters);
+return returnData;
+}
+///<summary>
+///将两个数相加
+///</summary>
+public static async Task<System.Int32> AddAsync<TClient>(this TClient client,System.Int32 a,System.Int32 b,IInvokeOption invokeOption = default) where TClient:
+TouchSocket.Rpc.IRpcClient{
+object[] parameters = new object[]{a,b};
+return (System.Int32) await client.InvokeAsync(typeof(System.Int32),"Add",invokeOption, parameters);
+}
 
-        ///<summary>
-        ///测试客户端请求，服务器响应大量流数据
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        public static System.Int32 RpcPullChannel<TClient>(this TClient client, System.Int32 channelID, IInvokeOption invokeOption = default) where TClient :
-        TouchSocket.Rpc.IRpcClient
-        {
-            var parameters = new object[] { channelID };
-            var returnData = (System.Int32)client.Invoke(typeof(System.Int32), "consoleapp2.program+myrpcserver.rpcpullchannel", invokeOption, parameters);
-            return returnData;
-        }
-        ///<summary>
-        ///测试客户端请求，服务器响应大量流数据
-        ///</summary>
-        public static async Task<System.Int32> RpcPullChannelAsync<TClient>(this TClient client, System.Int32 channelID, IInvokeOption invokeOption = default) where TClient :
-        TouchSocket.Rpc.IRpcClient
-        {
-            var parameters = new object[] { channelID };
-            return (System.Int32)await client.InvokeAsync(typeof(System.Int32), "consoleapp2.program+myrpcserver.rpcpullchannel", invokeOption, parameters);
-        }
+///<summary>
+///测试客户端请求，服务器响应大量流数据
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+public static System.Int32 RpcPullChannel<TClient>(this TClient client,System.Int32 channelID,IInvokeOption invokeOption = default) where TClient:
+TouchSocket.Rpc.IRpcClient{
+object[] parameters = new object[]{channelID};
+System.Int32 returnData=(System.Int32)client.Invoke(typeof(System.Int32),"consoleapp2.program+myrpcserver.rpcpullchannel",invokeOption, parameters);
+return returnData;
+}
+///<summary>
+///测试客户端请求，服务器响应大量流数据
+///</summary>
+public static async Task<System.Int32> RpcPullChannelAsync<TClient>(this TClient client,System.Int32 channelID,IInvokeOption invokeOption = default) where TClient:
+TouchSocket.Rpc.IRpcClient{
+object[] parameters = new object[]{channelID};
+return (System.Int32) await client.InvokeAsync(typeof(System.Int32),"consoleapp2.program+myrpcserver.rpcpullchannel",invokeOption, parameters);
+}
 
-        ///<summary>
-        ///测试客户端推送流数据
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        public static System.Int32 RpcPushChannel<TClient>(this TClient client, System.Int32 channelID, IInvokeOption invokeOption = default) where TClient :
-        TouchSocket.Rpc.IRpcClient
-        {
-            var parameters = new object[] { channelID };
-            var returnData = (System.Int32)client.Invoke(typeof(System.Int32), "consoleapp2.program+myrpcserver.rpcpushchannel", invokeOption, parameters);
-            return returnData;
-        }
-        ///<summary>
-        ///测试客户端推送流数据
-        ///</summary>
-        public static async Task<System.Int32> RpcPushChannelAsync<TClient>(this TClient client, System.Int32 channelID, IInvokeOption invokeOption = default) where TClient :
-        TouchSocket.Rpc.IRpcClient
-        {
-            var parameters = new object[] { channelID };
-            return (System.Int32)await client.InvokeAsync(typeof(System.Int32), "consoleapp2.program+myrpcserver.rpcpushchannel", invokeOption, parameters);
-        }
+///<summary>
+///测试客户端推送流数据
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+public static System.Int32 RpcPushChannel<TClient>(this TClient client,System.Int32 channelID,IInvokeOption invokeOption = default) where TClient:
+TouchSocket.Rpc.IRpcClient{
+object[] parameters = new object[]{channelID};
+System.Int32 returnData=(System.Int32)client.Invoke(typeof(System.Int32),"consoleapp2.program+myrpcserver.rpcpushchannel",invokeOption, parameters);
+return returnData;
+}
+///<summary>
+///测试客户端推送流数据
+///</summary>
+public static async Task<System.Int32> RpcPushChannelAsync<TClient>(this TClient client,System.Int32 channelID,IInvokeOption invokeOption = default) where TClient:
+TouchSocket.Rpc.IRpcClient{
+object[] parameters = new object[]{channelID};
+return (System.Int32) await client.InvokeAsync(typeof(System.Int32),"consoleapp2.program+myrpcserver.rpcpushchannel",invokeOption, parameters);
+}
 
-    }
+}
 }
---
+++ examples/Unity3d/UnityServerConsoleApp/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TouchSocket.Core;
+using TouchSocket.Dmtp;
+using TouchSocket.Dmtp.Rpc;
+using TouchSocket.Rpc;
+using TouchSocket.Sockets;
+
+namespace UnityServerConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //unitypackage在本级目录下。
+            //已发布的apk和exe客户端也在本级目录下。
+            StartTcpService(7789);
+            StartTcpRpcService(7790);
+            StartUdpService(7791);
+            Console.ReadKey();
+        }
+
+        private static void StartUdpService(int port)
+        {
+            var udpService = new UdpSession();
+            udpService.Received = (remote, byteBlock, requestInfo) =>
+            {
+                udpService.Send(remote, byteBlock);
+                Console.WriteLine($"收到：{Encoding.UTF8.GetString(byteBlock.Buffer, 0, byteBlock.Len)}");
+            };
+            udpService.Setup(new TouchSocketConfig()
+                 .SetBindIPHost(new IPHost(port))
+                 .SetUdpDataHandlingAdapter(() => new NormalUdpDataHandlingAdapter())//常规udp
+                                                                                     //.SetUdpDataHandlingAdapter(() => new UdpPackageAdapter())//Udp包模式，支持超过64k数据。
+                 .ConfigureContainer(a =>
+                 {
+                     a.AddConsoleLogger();//添加一个日志注入
+                 }))
+                 .Start();
+
+            udpService.Logger.Info($"UdpService已启动，端口：{port}");
+        }
+
+        private static void StartTcpRpcService(int port)
+        {
+            var service = new TcpDmtpService();
+            var config = new TouchSocketConfig()//配置
+                   .SetListenIPHosts(new IPHost[] { new IPHost(port) })
+                   .ConfigureContainer(a =>
+                   {
+                       a.AddConsoleLogger();//注册一个日志组
+                   })
+                   .ConfigurePlugins(a =>
+                   {
+                       a.UseDmtpRpc()
+                       .ConfigureRpcStore(store =>
+                       {
+                           store.RegisterServer<MyRpcServer>();
+#if DEBUG
+                           var code = store.GetProxyCodes("UnityRpcProxy", typeof(DmtpRpcAttribute));
+                           File.WriteAllText("../../../UnityRpcProxy.cs", code);
+#endif
+
+                       });
+
+                       a.Add<MyTcpRpcPlguin>();
+                   })
+                   .SetVerifyToken("Dmtp");
+
+            service.Setup(config)
+                .Start();
+
+            service.Logger.Info($"{service.GetType().Name}已启动，监听端口：{port}");
+        }
+
+        private static void StartTcpService(int port)
+        {
+            var service = new TcpService();
+            service.Setup(new TouchSocketConfig()//载入配置
+                .SetListenIPHosts(new IPHost[] { new IPHost(port) })
+                .SetTcpDataHandlingAdapter(() => new FixedHeaderPackageAdapter())
+                .ConfigurePlugins(a =>
+                {
+                    a.Add<MyPlguin>();//此处可以添加插件
+                })
+                .ConfigureContainer(a =>
+                {
+                    a.AddConsoleLogger();//添加一个日志注入
+                }))
+                .Start();//启动
+            service.Logger.Info($"Tcp服务器已启动，端口{port}");
+        }
+    }
+
+    internal class MyTcpRpcPlguin : PluginBase
+    {
+    }
+
+    internal class MyPlguin : PluginBase, ITcpConnectedPlugin<ISocketClient>, ITcpDisconnectedPlugin<ISocketClient>,ITcpReceivedPlugin<ISocketClient>
+    {
+  
+        async Task ITcpConnectedPlugin<ISocketClient>.OnTcpConnected(ISocketClient client, ConnectedEventArgs e)
+        {
+            client.Logger.Info($"客户端{client.GetInfo()}已连接");
+            await e.InvokeNext();
+        }
+
+        async Task ITcpDisconnectedPlugin<ISocketClient>.OnTcpDisconnected(ISocketClient client, DisconnectEventArgs e)
+        {
+            client.Logger.Info($"客户端{client.GetInfo()}已断开连接");
+            await e.InvokeNext();
+        }
+
+        async Task ITcpReceivedPlugin<ISocketClient>.OnTcpReceived(ISocketClient client, ReceivedDataEventArgs e)
+        {
+            client.Logger.Info($"接收到信息：{Encoding.UTF8.GetString(e.ByteBlock.Buffer, 0, e.ByteBlock.Len)}");
+            client.Send($"服务器已收到你发送的消息：{e.ByteBlock.ToString()}");
+            await e.InvokeNext();
+        }
+    }
+
+    /// <summary>
+    /// 单例服务
+    /// </summary>
+    public class MyRpcServer : RpcServer
+    {
+        public MyRpcServer(ILog logger)
+        {
+            this.m_timer = new Timer((obj) =>
+            {
+                logger.Info($"count={this.count}");
+            }, null, 0, 1000);
+            this.m_logger = logger;
+        }
+
+        private Timer m_timer;
+        private int count;
+        private readonly ILog m_logger;
+
+        [Description("登录")]
+        [DmtpRpc(true, MethodFlags = MethodFlags.IncludeCallContext)]
+        public MyLoginModelResult Login(ICallContext callContext, MyLoginModel model)
+        {
+            if (model.Account == "123" && model.Password == "abc")
+            {
+                return new MyLoginModelResult() { Status = 1, Message = "Success" };
+            }
+
+            return new MyLoginModelResult() { Status = 2, Message = "账号或密码错误" };
+        }
+
+        [Description("性能测试")]
+        [DmtpRpc(true)]
+        public int Performance(int i)
+        {
+            Interlocked.Increment(ref this.count);
+            return ++i;
+        }
+    }
+
+    public class MyLoginModel
+    {
+        public string Token { get; set; }
+        public string Account { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class MyLoginModelResult
+    {
+        public byte Status { get; set; }
+        public string Message { get; set; }
+    }
+}
