dbb9afcaf0501a40f9308c001bf37f1f6e41b1f6
若汝棋茗 <505554090@qq.com>
2023-09-17 20:22:29 +0800

    新增AOT示例

---
+++ examples_aot/Tcp/TcpConsoleApp/Program.cs
@@ -0,0 +1,263 @@
+﻿using System.Collections;
+using System.Text;
+using TouchSocket.Core;
+using TouchSocket.Sockets;
+
+namespace TcpConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //GlobalEnvironment.OptimizedPlatforms = OptimizedPlatforms.Unity;
+            var service = CreateService();
+            var client = CreateClient();
+            Console.WriteLine("输入任意内容，回车发送");
+            while (true)
+            {
+                client.Send(Console.ReadLine());
+            }
+        }
+
+        private static TcpService CreateService()
+        {
+            var service = new TcpService();
+            service.Connecting = (client, e) => { };//有客户端正在连接
+            service.Connected = (client, e) => { };//有客户端成功连接
+            service.Disconnected = (client, e) => { };//有客户端断开连接
+
+            service.Setup(new TouchSocketConfig()//载入配置
+
+                .SetContainer(new MyContainer())
+                .SetListenIPHosts("tcp://127.0.0.1:7789", 7790)//同时监听两个地址
+                .ConfigureContainer(a =>//容器的配置顺序应该在最前面
+                {
+                    a.AddConsoleLogger();//添加一个控制台日志注入（注意：在maui中控制台日志不可用）
+                    a.RegisterSingleton(service);//将服务器以单例注入。便于插件或其他地方获取。
+                })
+                .ConfigurePlugins(a =>
+                {
+                    a.Add<ClosePlugin>();
+                    a.Add<TcpServiceReceivedPlugin>();
+                    a.Add<MyServicePluginClass>();
+                    //a.Add();//此处可以添加插件
+                }))
+                .Start();//启动
+            return service;
+        }
+
+        private static TcpClient CreateClient()
+        {
+            var tcpClient = new TcpClient();
+            tcpClient.Connected = (client, e) => { };//成功连接到服务器
+            tcpClient.Disconnected = (client, e) => { };//从服务器断开连接，当连接不成功时不会触发。
+            tcpClient.Received = (client, byteBlock, requestInfo) =>
+            {
+                //从服务器收到信息
+                var mes = Encoding.UTF8.GetString(byteBlock.Buffer, 0, byteBlock.Len);
+                tcpClient.Logger.Info($"客户端接收到信息：{mes}");
+            };
+
+            //载入配置
+            tcpClient.Setup(new TouchSocketConfig()
+                .SetContainer(new MyContainer())
+                .SetRemoteIPHost(new IPHost("127.0.0.1:7789"))
+                .ConfigurePlugins(a =>
+                {
+                    a.UseReconnection()
+                    .SetTick(TimeSpan.FromSeconds(1))
+                    .UsePolling();
+                })
+                .ConfigureContainer(a =>
+                {
+                    a.AddConsoleLogger();//添加一个日志注入
+                }))
+                .Connect();
+            tcpClient.Logger.Info("客户端成功连接");
+            return tcpClient;
+        }
+    }
+
+    /// <summary>
+    /// IOC容器
+    /// </summary>
+    public class MyContainer : IContainer
+    {
+        public MyContainer()
+        {
+            this.m_pluginsManager = new PluginsManager(this);
+            this.m_logger = new LoggerGroup();
+            this.m_closePlugin = new ClosePlugin(this.m_logger);
+        }
+
+        readonly IPluginsManager m_pluginsManager;
+        readonly ILog m_logger;
+        readonly ClosePlugin m_closePlugin;
+        readonly TcpServiceReceivedPlugin m_tcpServiceReceivedPlugin = new TcpServiceReceivedPlugin();
+        readonly MyServicePluginClass m_myServicePluginClass = new MyServicePluginClass();
+
+        public bool IsRegistered(Type fromType, string key = "")
+        {
+            //一般所有的类型，都返回true
+            return true;
+        }
+
+        
+        public void Register(DependencyDescriptor descriptor, string key = "")
+        {
+            //一般不用实现
+        }
+
+        public object Resolve(Type fromType, object[] ps = null, string key = "")
+        {
+            //此处的逻辑主要实现，当容器请求类型时，返回对应的实例。
+            //如果是单例，可以将单例保存为字段，直接返回
+            //如果是瞬态，则可以返回new
+            if ($"{fromType.FullName}{key}" == $"{typeof(IPluginsManager).FullName}{key}")
+            {
+                return this.m_pluginsManager;
+            }
+            else if ($"{fromType.FullName}{key}" == $"{typeof(ILog).FullName}{key}")
+            {
+                return this.m_logger;
+            }
+            else if ($"{fromType.FullName}{key}" == $"{typeof(TouchSocket.Core.IContainer).FullName}{key}")
+            {
+                return this;
+            }
+            else if ($"{fromType.FullName}{key}" == $"{typeof(ClosePlugin).FullName}{key}")
+            {
+                return this.m_closePlugin;
+            }
+            else if ($"{fromType.FullName}{key}" == $"{typeof(TcpServiceReceivedPlugin).FullName}{key}")
+            {
+                return this.m_tcpServiceReceivedPlugin;
+            }
+            else if ($"{fromType.FullName}{key}" == $"{typeof(MyServicePluginClass).FullName}{key}")
+            {
+                return this.m_myServicePluginClass;
+            }
+            throw new Exception("没有解决容器类型");
+        }
+
+       
+        public void Unregister(DependencyDescriptor descriptor, string key = "")
+        {
+            //一般不用实现
+        }
+
+        IEnumerator<DependencyDescriptor> IEnumerable<DependencyDescriptor>.GetEnumerator()
+        {
+            //一般不用实现
+            return default;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            //一般不用实现
+            return ((TouchSocket.Core.IContainer)this).GetEnumerator();
+        }
+    }
+
+    internal partial class MyServicePluginClass : PluginBase
+    {
+        [GeneratorPlugin(nameof(IServerStartedPlugin.OnServerStarted))]
+        public Task OnServerStarted(IService sender, ServiceStateEventArgs e)
+        {
+            if (sender is ITcpService service)
+            {
+                foreach (var item in service.Monitors)
+                {
+                    ConsoleLogger.Default.Info($"iphost={item.Option.IpHost}");
+                }
+            }
+            if (e.ServerState == ServerState.Running)
+            {
+                ConsoleLogger.Default.Info($"服务器成功启动");
+            }
+            else
+            {
+                ConsoleLogger.Default.Info($"服务器启动失败，状态：{e.ServerState}，异常：{e.Exception}");
+            }
+            return e.InvokeNext();
+        }
+
+        [GeneratorPlugin(nameof(IServerStopedPlugin.OnServerStoped))]
+        public Task OnServerStoped(IService sender, ServiceStateEventArgs e)
+        {
+            Console.WriteLine("服务已停止");
+            return e.InvokeNext();
+        }
+    }
+
+    partial class TcpServiceReceivedPlugin : PluginBase
+    {
+        [GeneratorPlugin(nameof(ITcpReceivedPlugin.OnTcpReceived))]
+        public async Task OnTcpReceived(ISocketClient client, ReceivedDataEventArgs e)
+        {
+            //从客户端收到信息
+            var mes = Encoding.UTF8.GetString(e.ByteBlock.Buffer, 0, e.ByteBlock.Len);
+            if (mes == "close")
+            {
+                throw new CloseException(mes);
+            }
+            client.Logger.Info($"已从{client.GetIPPort()}接收到信息：{mes}");
+
+            client.Send(mes);//将收到的信息直接返回给发送方
+
+            //client.Send("id",mes);//将收到的信息返回给特定ID的客户端
+
+            //注意，此处是使用的当前客户端的接收线程做发送，实际使用中不可以这样做。不然一个客户端阻塞，将导致本客户端无法接收数据。
+            var ids = client.Service.GetIds();
+            foreach (var clientId in ids)//将收到的信息返回给在线的所有客户端。
+            {
+                if (clientId != client.Id)//不给自己发
+                {
+                    await client.Service.SendAsync(clientId, mes);
+                }
+            }
+
+            //await Task.Delay(1000);
+        }
+    }
+
+    /// <summary>
+    /// 应一个网友要求，该插件主要实现，在接收数据时如果触发<see cref="CloseException"/>异常，则断开连接。
+    /// </summary>
+    partial class ClosePlugin : PluginBase
+    {
+        private readonly ILog m_logger;
+
+        public ClosePlugin(ILog logger)
+        {
+            this.m_logger = logger;
+        }
+
+        [GeneratorPlugin(nameof(ITcpReceivedPlugin.OnTcpReceived))]
+        public async Task OnTcpReceived(ISocketClient client, ReceivedDataEventArgs e)
+        {
+            try
+            {
+                await e.InvokeNext();
+            }
+            catch (CloseException ex)
+            {
+                m_logger.Info("拦截到CloseException");
+                client.Close(ex.Message);
+            }
+            catch (Exception exx)
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+    }
+
+    class CloseException : Exception
+    {
+        public CloseException(string msg) : base(msg) { }
+    }
+}
