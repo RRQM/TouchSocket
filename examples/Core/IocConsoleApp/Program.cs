28d274780908162d95961a0484e9b8c6becd1a3b
若汝棋茗 <505554090@qq.com>
2023-09-18 17:32:18 +0800

    更新文档

--- examples/Core/IocConsoleApp/Program.cs
+++ examples/Core/IocConsoleApp/Program.cs
@@ -12,6 +12,7 @@ namespace IocConsoleApp
             ConstructorInject();
             PropertyInject();
             MethodInject();
+            DefaultParmaterInject();
             Console.ReadKey();
         }
 
@@ -30,6 +31,9 @@ namespace IocConsoleApp
             Console.WriteLine(MethodBase.GetCurrentMethod().Name);
         }
 
+        /// <summary>
+        /// 属性注入
+        /// </summary>
         static void PropertyInject()
         {
             var container = GetContainer();
@@ -43,25 +47,48 @@ namespace IocConsoleApp
             Console.WriteLine(MethodBase.GetCurrentMethod().Name);
         }
 
+        /// <summary>
+        /// 方法注入
+        /// </summary>
         static void MethodInject()
         {
             var container = GetContainer();
             container.RegisterSingleton<MyClass1>();
             container.RegisterSingleton<MyClass4>();
 
-            var myClass4= container.Resolve<MyClass4>();
+            var myClass4 = container.Resolve<MyClass4>();
             Console.WriteLine(MethodBase.GetCurrentMethod().Name);
+        }
+
+        /// <summary>
+        /// 默认参数注入
+        /// </summary>
+        static void DefaultParmaterInject()
+        {
+            var container = GetContainer();
+            container.RegisterSingleton<MyClass1>();
+            container.RegisterTransient<MyClass5>();//默认参数只在瞬时生命有效
+
+            var myClass5_1 = container.Resolve<MyClass5>(new object[] { 10, "hello" });
+            var myClass5_2 = container.Resolve<MyClass5>(new object[] { 20, "hi" });
+            var myClass5_3 = container.Resolve<MyClass5>(new object[] { 20, "hi", new MyClass1() });
+
+            //以下结果为true，因为在构建时并未覆盖MyClass1，所以MyClass1只能从容器获得，而MyClass1在容器是以单例注册，所以相同
+            var b1 = myClass5_1.MyClass1 == myClass5_2.MyClass1;
+
+            //以下结果为false，因为在构建时覆盖MyClass1，所以MyClass1是从ps参数直接传递的，所以不相同
+            var b2 = myClass5_1.MyClass1 == myClass5_3.MyClass1;
         }
 
         static IContainer GetContainer()
         {
-            //return new Container();//默认IOC容器
+            return new Container();//默认IOC容器
 
-            return new AspNetCoreContainer(new ServiceCollection());//使用Aspnetcore的容器
+            //return new AspNetCoreContainer(new ServiceCollection());//使用Aspnetcore的容器
         }
     }
 
-   
+
     class MyClass1
     {
 
@@ -100,7 +127,7 @@ namespace IocConsoleApp
 
     class MyClass4
     {
-        public MyClass1 MyClass1 { get;private set; }
+        public MyClass1 MyClass1 { get; private set; }
 
 
         [DependencyInject]
@@ -108,5 +135,19 @@ namespace IocConsoleApp
         {
             this.MyClass1 = myClass1;
         }
+    }
+
+    class MyClass5
+    {
+        public MyClass5(int a, string b, MyClass1 myClass1)
+        {
+            this.A = a;
+            this.B = b;
+            this.MyClass1 = myClass1;
+        }
+
+        public int A { get; }
+        public string B { get; }
+        public MyClass1 MyClass1 { get; }
     }
 }
