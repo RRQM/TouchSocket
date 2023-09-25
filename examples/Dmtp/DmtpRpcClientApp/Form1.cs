f158190bb65ecc2479581e55458e142cfde9a8e6
若汝棋茗 <505554090@qq.com>
2023-09-25 11:59:53 +0800

    更新引用

--- examples/Dmtp/DmtpRpcClientApp/Form1.cs
+++ examples/Dmtp/DmtpRpcClientApp/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using RpcProxy;
 using System;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using TouchSocket.Core;
 using TouchSocket.Dmtp;
@@ -48,6 +49,39 @@ namespace DmtpClientApp
                 .ConfigurePlugins(a =>
                 {
                     a.UseDmtpRpc();
+
+                    //使用心跳保活，或者避免异常连接。达到最大失败次数会断开，不会重连。
+                    a.UseDmtpHeartbeat()
+                    .SetTick(TimeSpan.FromSeconds(3))
+                    .SetMaxFailCount(3);
+
+                    //使用重连
+                    a.UseReconnection<TcpDmtpClient>()
+                    .SetActionForCheck(async (c, i) =>//重新定义检活策略
+                    {
+                        //方法1，直接判断是否在握手状态。使用该方式，最好和心跳插件配合使用
+                        //await Task.CompletedTask;//消除Task
+                        //return c.IsHandshaked;//判断是否在握手状态
+
+                        //方法2，直接ping，如果true，则客户端必在线。如果false，则客户端不一定不在线，原因是可能当前传输正在忙
+                        if (await c.PingAsync())
+                        {
+                            return true;
+                        }
+                        //返回false时可以判断，如果最近活动时间不超过3秒，则猜测客户端确实在忙，所以跳过本次重连
+                        else if (DateTime.Now- c.GetLastActiveTime()<TimeSpan.FromSeconds(3))
+                        {
+                            return null;
+                        }
+                        //否则，直接重连。
+                        else
+                        {
+                            return false;
+                        }
+                    })
+                    .SetTick(TimeSpan.FromSeconds(3))
+                    .UsePolling();
+
                 })
                 .SetVerifyToken("Rpc"));
             this.client.Connect();
