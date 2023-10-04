89b7987319772e919e83bac21b038d9e7ba855be
若汝棋茗 <505554090@qq.com>
2023-10-04 12:19:42 +0800

    更新引用

---
+++ examples/JsonRpc/ReverseJsonRpcConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ReverseJsonRpcConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
