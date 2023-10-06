b2ab24250f6f6f6ca7dee6459ee9237965ae94e5
若汝棋茗 <505554090@qq.com>
2023-10-07 00:00:31 +0800

    更新demo

--- examples/Dmtp/DmtpRpcClientApp/Form1.Designer.cs
+++ examples/Dmtp/DmtpRpcClientApp/Form1.Designer.cs
@@ -37,6 +37,9 @@ namespace DmtpClientApp
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,7 +57,7 @@ namespace DmtpClientApp
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 93);
+            this.label1.Location = new System.Drawing.Point(24, 147);
             this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(86, 31);
@@ -63,7 +66,7 @@ namespace DmtpClientApp
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(108, 88);
+            this.textBox1.Location = new System.Drawing.Point(106, 142);
             this.textBox1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(196, 38);
@@ -72,7 +75,7 @@ namespace DmtpClientApp
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(454, 88);
+            this.textBox2.Location = new System.Drawing.Point(452, 142);
             this.textBox2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(196, 38);
@@ -82,7 +85,7 @@ namespace DmtpClientApp
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(372, 93);
+            this.label2.Location = new System.Drawing.Point(370, 147);
             this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(86, 31);
@@ -125,11 +128,44 @@ namespace DmtpClientApp
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Tcp调用";
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(24, 54);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 46);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "连接";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += this.button4_Click;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(204, 54);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 46);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "断开连接";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += this.button5_Click;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(385, 54);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(150, 46);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "释放";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += this.button6_Click;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1192, 294);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
@@ -154,6 +190,9 @@ namespace DmtpClientApp
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
