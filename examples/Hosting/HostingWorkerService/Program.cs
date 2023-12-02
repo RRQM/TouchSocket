33f4c46f0e94ceca2e41a343ffddc514f0b3b207
若汝棋茗 <505554090@qq.com>
2023-12-02 22:34:28 +0800

    构建文档

---
+++ examples/Hosting/HostingWorkerService/Program.cs
@@ -0,0 +1,17 @@
+namespace HostingWorkerService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
