ec290c929a65f759c891446d8eec77114f621d12
huanghr <youthalan@126.com>
2023-12-13 22:26:13 +0800

    添加iXCom29s协议(主要用于某行业L2和MES通讯)

---
+++ src/TouchSocket.ProtocolHouse/XCom29s/Dtos/TelegramListDto.cs
@@ -0,0 +1,48 @@
+﻿namespace TouchSocket.ProtocolHouse;
+
+public class TelegramListDto
+{
+    #region 实体属性
+
+    /// <summary>
+    /// 电文号
+    /// </summary>
+    public virtual string TelegramNo { get; set; }
+
+    /// <summary>
+    /// 电文名称
+    /// </summary>
+    public virtual string TelegramName { get; set; }
+
+    /// <summary>
+    /// 英文名称
+    /// </summary>
+    public virtual string TelegramTableName { get; set; }
+
+    /// <summary>
+    /// 发送/接收
+    /// </summary>
+    public virtual string CommunicationType { get; set; }
+
+    /// <summary>
+    /// 外部系统
+    /// </summary>
+    public virtual string ExternalSystem { get; set; }
+
+    /// <summary>
+    /// 电文类型
+    /// </summary>
+    public virtual string TelegramType { get; set; }
+
+    /// <summary>
+    /// 电文长度
+    /// </summary>
+    public virtual int TelegramLength { get; set; }
+
+    /// <summary>
+    /// 备注
+    /// </summary>
+    public virtual string Remarks { get; set; }
+
+    #endregion 实体属性
+}
