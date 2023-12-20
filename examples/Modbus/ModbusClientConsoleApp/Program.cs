078975c07d0e09af9f1c81a402da3d0e7ad00014
若汝棋茗 <505554090@qq.com>
2023-12-20 16:08:46 +0800

    更新文档

--- examples/Modbus/ModbusClientConsoleApp/Program.cs
+++ examples/Modbus/ModbusClientConsoleApp/Program.cs
@@ -1,10 +1,139 @@
-﻿namespace ModbusClientConsoleApp
+﻿using TouchSocket.Core;
+using TouchSocket.Modbus;
+using TouchSocket.SerialPorts;
+using TouchSocket.Sockets;
+
+namespace ModbusClientConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var client = GetModbusTcpClient();
+
+            ReadWriteHoldingRegisters(client);
+            Console.ReadKey();
         }
+
+        /// <summary>
+        /// 要测试，请打开Modbus Slave软件，设置HoldingRegisters。至少30个长度。
+        /// </summary>
+        public static void ReadWriteHoldingRegisters(IModbusClient client)
+        {
+            //写入单个寄存器
+            client.WriteSingleRegister(1, 0, 1);//默认short ABCD端序
+            client.WriteSingleRegister(1, 1, 1000);//默认short ABCD端序
+
+            using (var valueByteBlock = new ValueByteBlock(1024))
+            {
+                valueByteBlock.Write((ushort)2, EndianType.Big);//ABCD端序
+                valueByteBlock.Write((ushort)2000, EndianType.Little);//DCBA端序
+                valueByteBlock.Write(int.MaxValue, EndianType.BigSwap);//BADC端序
+                valueByteBlock.Write(long.MaxValue, EndianType.LittleSwap);//CDAB端序
+
+                //写入字符串，会先用4字节表示字符串长度，然后按utf8编码写入字符串
+                valueByteBlock.Write("Hello");
+
+                //写入任意类型，使用Json序列化
+                valueByteBlock.WriteObject(new MyClass() { P1 = 1, P2 = 2 }, SerializationType.Json);
+
+                //写入到寄存器
+                client.WriteMultipleRegisters(1, 2, valueByteBlock.ToArray());
+            }
+
+            //读取寄存器
+            var response = client.ReadHoldingRegisters(1, 0, 30);
+
+            //创建一个读取器
+            var reader = response.CreateReader();
+
+            Console.WriteLine(reader.ReadInt16(EndianType.Big));
+            Console.WriteLine(reader.ReadInt16(EndianType.Big));
+            Console.WriteLine(reader.ReadInt16(EndianType.Big));
+            Console.WriteLine(reader.ReadInt16(EndianType.Little));
+            Console.WriteLine(reader.ReadInt32(EndianType.BigSwap));
+            Console.WriteLine(reader.ReadInt64(EndianType.LittleSwap));
+            Console.WriteLine(reader.ReadString());
+            Console.WriteLine(reader.ReadObject<MyClass>(SerializationType.Json).ToJsonString());
+
+        }
+
+        /// <summary>
+        /// 读写线圈，在测试时，请选择对应的Modbus Slave类型，且调到线圈操作，至少5个长度
+        /// </summary>
+        /// <param name="client"></param>
+        public static void ReadWriteCoilsShouldBeOk(IModbusClient client)
+        {
+            //写单个线圈
+            client.WriteSingleCoil(1, 0, true);
+            client.WriteSingleCoil(1, 1, false);
+
+            //写多个线圈
+            client.WriteMultipleCoils(1, 2, new bool[] { true, false, true });
+
+            //读取线圈
+            var values = client.ReadCoils(1, 0, 5);
+            foreach (var value in values)
+            {
+                Console.WriteLine(value);
+            }
+        }
+
+        public static IModbusTcpClient GetModbusTcpClient()
+        {
+            var client = new ModbusTcpClient();
+
+            client.Connect("127.0.0.1:502");
+            return client;
+        }
+
+        public static IModbusClient GetModbusUdpClient()
+        {
+            var client = new ModbusUdpClient();
+            client.Setup(new TouchSocketConfig()
+                .UseUdpReceive()
+                .SetRemoteIPHost("127.0.0.1:502"));
+            client.Start();
+            return client;
+        }
+
+        public static IModbusClient GetModbusRtuClient()
+        {
+            var client = new ModbusRtuClient();
+            client.Setup(new TouchSocketConfig()
+                .SetSerialPortOption(new SerialPortOption()
+                {
+                    BaudRate = 9600,
+                    DataBits = 8,
+                    Parity = System.IO.Ports.Parity.Even,
+                    PortName = "COM2",
+                    StopBits = System.IO.Ports.StopBits.One
+                }));
+            client.Connect();
+            return client;
+        }
+
+        public static IModbusClient GetModbusRtuOverTcpClient()
+        {
+            var client = new ModbusRtuOverTcpClient();
+            client.Connect("127.0.0.1:502");
+            return client;
+        }
+
+        public static IModbusClient GetModbusRtuOverUdpClient()
+        {
+            var client = new ModbusRtuOverUdpClient();
+            client.Setup(new TouchSocketConfig()
+                .UseUdpReceive()
+                .SetRemoteIPHost("127.0.0.1:502"));
+            client.Start();
+            return client;
+        }
+    }
+
+    class MyClass
+    {
+        public int P1 { get; set; }
+        public int P2 { get; set; }
     }
 }
