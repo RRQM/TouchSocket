9fd4e2deb293403e0e02efad506b3b0a06870de5
若汝棋茗 <505554090@qq.com>
2023-12-22 22:58:58 +0800

    更新引用

---
+++ examples/Modbus/ModbusSlaveConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ModbusSlaveConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
