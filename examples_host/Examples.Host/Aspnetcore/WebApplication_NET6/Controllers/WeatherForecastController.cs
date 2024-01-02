34721a88a7693c2230bb3f8ef736ee7c649cb02d
若汝棋茗 <505554090@qq.com>
2024-01-02 17:33:06 +0800

    更新demo

--- examples_host/Examples.Host/Aspnetcore/WebApplication_NET6/Controllers/WeatherForecastController.cs
+++ examples_host/Examples.Host/Aspnetcore/WebApplication_NET6/Controllers/WeatherForecastController.cs
@@ -18,16 +18,5 @@ namespace WebApplication_NET6.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
     }
 }
