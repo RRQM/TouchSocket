13051fea92c83a412992a2d2353a50305f75ab60
若汝棋茗 <505554090@qq.com>
2024-01-02 17:33:06 +0800

    更新demo

--- examples_host/Examples.Host/BackgroundService/WorkerService_NET6/Program.cs
+++ examples_host/Examples.Host/BackgroundService/WorkerService_NET6/Program.cs
@@ -7,7 +7,7 @@ namespace WorkerService_NET6
     {
         public static void Main(string[] args)
         {
-            IHost host = Host.CreateDefaultBuilder(args)
+            var builder = Host.CreateDefaultBuilder(args)
                 .ConfigureServices(services =>
                 {
                     //添加Tcp服务器。默认是单例
@@ -21,20 +21,20 @@ namespace WorkerService_NET6
                         config.SetRemoteIPHost("127.0.0.1:7789");
                     });
 
-                    services.AddTcpService<ITcpDmtpService, TcpDmtpService>(config => 
+                    services.AddTcpService<ITcpDmtpService, TcpDmtpService>(config =>
                     {
                         config.SetListenIPHosts(8848);
                     });
 
-                    //添加瞬态Tcp服务器。
-                    //瞬态服务器，在host启动时，不会自己start，需要手动调用
-                    services.AddScopedSetupConfigObject<ITcpService, TcpService>(config =>
-                    {
-                        config.SetListenIPHosts(7789);
-                    });
-                    services.AddHostedService<Worker>();
-                })
-                .Build();
+                    ////添加瞬态Tcp服务器。
+                    ////瞬态服务器，在host启动时，不会自己start，需要手动调用
+                    //services.AddScopedSetupConfigObject<ITcpService, TcpService>(config =>
+                    //{
+                    //    config.SetListenIPHosts(7789);
+                    //});
+                });
+
+            var host = builder.Build();
 
             host.Run();
         }
