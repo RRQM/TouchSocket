0d0ffb4717361ada59aa23fecb80662d7376213f
若汝棋茗 <505554090@qq.com>
2024-01-12 23:40:18 +0800

    优化http性能

---
+++ performancetest/Http/HttpPerformanceConsoleApp/Controllers/FastApi.cs
@@ -0,0 +1,19 @@
+﻿using BeetleX.FastHttpApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpPerformanceConsoleApp.Controllers
+{
+    [Controller]
+    internal class FastApi
+    {
+        [Get(Route = "ApiServer/Add")]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
