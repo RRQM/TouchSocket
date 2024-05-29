1f27522538af0bf9187135ec237df5b25a58c2b7
若汝棋茗 <505554090@qq.com>
2024-05-29 14:03:32 +0800

    2.1.0-alpha.14

---
+++ src/TouchSocket.Shared/Sockets/ThrowHelperSockets.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using TouchSocket.Resources;
+using TouchSocket.Sockets;
+
+namespace TouchSocket.Core
+{
+    internal static partial class ThrowHelper
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ThrowClientNotConnectedException()
+        {
+            throw new ClientNotConnectedException(TouchSocketResource.ClientNotConnected);
+        }
+        
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ThrowClientNotFindException(string id)
+        {
+            throw new ClientNotFindException(TouchSocketResource.ClientNotFind.Format(id));
+        }
+    }
+}
