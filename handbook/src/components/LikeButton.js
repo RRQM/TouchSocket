4c81e994d9dd2975f252619a4dfddff43892aaf4
若汝棋茗 <505554090@qq.com>
2024-08-13 23:56:44 +0800

    文档：完善点赞功能

--- handbook/src/components/LikeButton.js
+++ handbook/src/components/LikeButton.js
@@ -7,13 +7,37 @@ const MyApp = () => {
   const [likeCount, setLikeCount] = useState(0); // 使用 useState 来管理计数
 
   useEffect(() => {
-    console.log(`Component rendered at ${location.pathname}`);
+    handleLikeClick();
   }, [location.pathname]); // 添加依赖项以确保只在路由变化时重新运行
 
-  const handleLikeClick = () => {
-    setLikeCount(likeCount + 1); // 使用 setState 方法更新计数
-    console.log(`User liked the page. New like count: ${likeCount + 1}`);
+  const handleLikeClick =async () => 
+  {
+    try {
+    //   const response = await fetch('http://127.0.0.1:7789/likebuttonserver/linkclick', {
+    //     method: 'POST', // 或者 'PUT'
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(
+    //       {
+    //         "link": location.pathname,
+    //       }), // 数据体必须是 JSON 格式
+    //   });
+
+    //   if (!response.ok) {
+    //     throw new Error(`HTTP error! Status: ${response.status}`);
+    //   }
+
+    //   const responseData = await response.json();
+    //   setLikeCount(responseData.Count); // 使用 setState 方法更新计数
+    } 
+    catch (error) 
+    {
+      console.error('Error:', error);
+    }
   };
+
+  
 
   return (
     <BrowserOnly>
