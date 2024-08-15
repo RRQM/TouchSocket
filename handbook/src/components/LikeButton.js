77e4c799b9501868b254248e07791de7b1ea6e50
若汝棋茗 <505554090@qq.com>
2024-08-15 12:04:36 +0800

    文档：完善点赞功能

--- handbook/src/components/LikeButton.js
+++ handbook/src/components/LikeButton.js
@@ -2,47 +2,109 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 
-const MyApp = () => {
+const MyApp = () =>
+{
   const location = useLocation();
-  const [likeCount, setLikeCount] = useState(0); // 使用 useState 来管理计数
+  const [likeCount, setLikeCount] = useState(0);
+  const [canLike, setCanLike] = useState(false); // 新增的状态用于控制是否可以点赞
 
-  useEffect(() => {
-    handleLikeClick();
-  }, [location.pathname]); // 添加依赖项以确保只在路由变化时重新运行
-
-  const handleLikeClick =async () => 
+  useEffect(() =>
   {
-    try {
-    //   const response = await fetch('http://127.0.0.1:7789/likebuttonserver/linkclick', {
-    //     method: 'POST', // 或者 'PUT'
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify(
-    //       {
-    //         "link": location.pathname,
-    //       }), // 数据体必须是 JSON 格式
-    //   });
+    let timer;
+    if (location.pathname)
+    {
+      timer = setTimeout(() =>
+      {
+        setCanLike(true); // 10秒后允许点赞
+      }, 10000); // 10000毫秒 = 10秒
+    }
 
-    //   if (!response.ok) {
-    //     throw new Error(`HTTP error! Status: ${response.status}`);
-    //   }
+    // 清理函数
+    return () => clearTimeout(timer);
+  }, [location.pathname]);
 
-    //   const responseData = await response.json();
-    //   setLikeCount(responseData.Count); // 使用 setState 方法更新计数
-    } 
-    catch (error) 
+  const handleLikeClick = async () =>
+  {
+    if (!canLike)
+    {
+      alert('Please read for at least 10 seconds before liking.');
+      return;
+    }
+
+    try
+    {
+      const response = await fetch('https://touchsocket.net/likebuttonserver/linkclick', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "link": location.pathname,
+        }),
+      });
+
+      if (!response.ok)
+      {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const responseData = await response.json();
+
+      if (responseData.IsSuccess)
+      {
+        setLikeCount(responseData.Count);
+      }
+      else
+      {
+        alert('aaa，太快了，歇歇啊！');
+      }
+    } catch (error)
     {
       console.error('Error:', error);
     }
   };
 
-  
+  const getLikeClick = async () =>
+  {
+    try
+    {
+      const response = await fetch('https://touchsocket.net/likebuttonserver/getLinkCount', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "link": location.pathname,
+        }),
+      });
+
+      if (!response.ok)
+      {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const responseData = await response.json();
+      setLikeCount(responseData.Count);
+    } catch (error)
+    {
+      console.error('Error:', error);
+    }
+  };
+
+  useEffect(() =>
+  {
+    getLikeClick();
+  }, [location.pathname]);
 
   return (
     <BrowserOnly>
       {() => (
-        <button className="like-button" onClick={handleLikeClick}>
+        <button
+          className="like-button"
+          onClick={handleLikeClick}
+          disabled={!canLike} // 如果不可以点赞，则禁用按钮
+          title={!canLike ? '请先阅读10秒后再点赞' : undefined}
+        >
           👍 {likeCount}
         </button>
       )}
