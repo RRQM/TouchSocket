64d2755b2b1a420c7d38e3e959ac16ba9740b1ed
若汝棋茗 <505554090@qq.com>
2024-09-25 13:40:30 +0800

    新增：ava示例

---
+++ examples/AvaloniaApplication/Client/AvaloniaApplication/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace AvaloniaApplication.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
