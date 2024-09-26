f96e981889b8ab749bee178b63b1d3ae4be49263
若汝棋茗 <505554090@qq.com>
2024-09-26 10:36:35 +0800

    更新：demo

--- examples/AvaloniaApplication/Client/AvaloniaApplication/ViewModels/MainViewModel.cs
+++ examples/AvaloniaApplication/Client/AvaloniaApplication/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using System;
+using System.Diagnostics;
 using System.Runtime.Intrinsics.Arm;
 using System.Threading.Tasks;
 using TouchSocket.Core;
@@ -16,7 +17,10 @@ public partial class MainViewModel : ViewModelBase
     public MainViewModel()
     {
         this.LoginCommand = new RelayCommand(this.Login);
+        this.Login1wCommand = new RelayCommand(this.Login1w);
     }
+
+    
 
     private string message;
 
@@ -24,6 +28,22 @@ public partial class MainViewModel : ViewModelBase
     {
         get { return message; }
         set { SetProperty(ref message, value); }
+    }
+
+    private string account;
+
+    public string Account
+    {
+        get { return account; }
+        set { SetProperty(ref account, value); }
+    }
+
+    private string password;
+
+    public string Password
+    {
+        get { return password; }
+        set { SetProperty(ref password, value); }
     }
 
 
@@ -44,13 +64,11 @@ public partial class MainViewModel : ViewModelBase
                  .SetRemoteIPHost("ws://localhost:5043/WebSocketDmtp"));
             await websocketDmtpClient.ConnectAsync();
 
-            this.Message = "login success";
+            this.Log("login success");
 
-            await Task.Delay(1000);
+            var b = await websocketDmtpClient.GetDmtpRpcActor().LoginAsync(this.account, this.password);
 
-            var b = await websocketDmtpClient.GetDmtpRpcActor().LoginAsync("account", "pwd");
-
-            this.Message = $"login result={b}";
+            this.Log($"login result={b}");
         }
         catch (Exception ex)
         {
@@ -59,5 +77,51 @@ public partial class MainViewModel : ViewModelBase
 
     }
 
+    private async void Login1w()
+    {
+        try
+        {
+            using var websocketDmtpClient = new WebSocketDmtpClient();
+            await websocketDmtpClient.SetupAsync(new TouchSocketConfig()
+                 .SetDmtpOption(new DmtpOption()
+                 {
+                     VerifyToken = "Dmtp"
+                 })
+                 .ConfigurePlugins(a =>
+                 {
+                     a.UseDmtpRpc();
+                 })
+                 .SetRemoteIPHost("ws://localhost:5043/WebSocketDmtp"));
+            await websocketDmtpClient.ConnectAsync();
+
+            this.Log("login success");
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                var b = await websocketDmtpClient.GetDmtpRpcActor().LoginAsync(this.account, this.password);
+                if (i%100==0)
+                {
+                    this.Log(i.ToString());
+                }
+            }
+            
+            stopwatch.Stop();
+
+            this.Log($"login result={stopwatch.Elapsed}");
+        }
+        catch (Exception ex)
+        {
+            this.Message = ex.Message;
+        }
+    }
+
+    private void Log(string msg)
+    {
+        this.Message = $"{msg}\r\n{this.message}";
+    }
+
     public RelayCommand LoginCommand { get; set; }
+    public RelayCommand Login1wCommand { get; set; }
 }
