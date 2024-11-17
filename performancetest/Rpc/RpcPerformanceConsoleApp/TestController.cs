a319d6975696a0039075e959735689c2cf7a799a
若汝棋茗 <505554090@qq.com>
2024-11-17 20:31:24 +0800

    更新：示例demo引用

--- performancetest/Rpc/RpcPerformanceConsoleApp/TestController.cs
+++ performancetest/Rpc/RpcPerformanceConsoleApp/TestController.cs
@@ -4,54 +4,19 @@ using TouchSocket.Rpc;
 
 namespace RpcPerformanceConsoleApp
 {
-    public interface ITestTaskController
-    {
-        Task<int> Sum(int a, int b);
-
-        Task<byte[]> GetBytes(int length);
-
-        Task<string> GetBigString();
-    }
-
-    public class TestTaskController : ITestTaskController
-    {
-        [DmtpRpc(true)]
-        public Task<int> Sum(int a, int b)
-        {
-            return Task.FromResult(a + b);
-        }
-
-        [DmtpRpc(true)]
-        public Task<byte[]> GetBytes(int length)
-        {
-            return Task.FromResult(new byte[length]);
-        }
-
-        [DmtpRpc(true)]
-        public Task<string> GetBigString()
-        {
-            var stringBuilder = new StringBuilder();
-            for (var i = 0; i < 10; i++)
-            {
-                stringBuilder.Append("RRQM");
-            }
-            return Task.FromResult(stringBuilder.ToString());
-        }
-    }
-
-
     public partial class TestController : RpcServer
     {
-        [DmtpRpc(true)]
+      
+        [DmtpRpc(MethodInvoke = true)]
         public int Sum(int a, int b) => a + b;
 
-        [DmtpRpc(true)]
+        [DmtpRpc(MethodInvoke = true)]
         public byte[] GetBytes(int length)
         {
             return new byte[length];
         }
 
-        [DmtpRpc(true)]
+        [DmtpRpc(MethodInvoke = true)]
         public string GetBigString()
         {
             var stringBuilder = new StringBuilder();
