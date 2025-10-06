f20ad06f5ad11ffd7edbbf6faf4b8a951bed0a80
若汝棋茗 <505554090@qq.com>
2025-10-06 15:25:18 +0800

    重构(RpcProxy): 重构RpcProxy代码结构和功能
    对RpcProxy.cs文件进行重构，优化接口和类定义，新增扩展方法以增强功能，调整代码格式以提升可读性
    - 删除版权声明注释，调整using语句顺序
    - 重新定义IXmlServer接口，新增[AsyncToSyncWarning]特性
    - 重构XmlServer类，保留Sum和TestClass方法及其异步版本
    - 新增XmlServerExtensions静态类，提供扩展方法Sum和TestClass
    - 移动MyClass类至文件末尾
    
    注释掉docusaurus.config.ts中的future配置项，暂时禁用Docusaurus v4相关功能
    - 注释v4和experimental_faster设置
    
    更新package.json依赖，支持性能优化
    - 新增@docusaurus/faster依赖，版本^3.9.1
    - 删除overrides配置，移除对特定依赖版本的覆盖

--- examples/XmlRpc/XmlRpcServerApp/RpcProxy.cs
+++ examples/XmlRpc/XmlRpcServerApp/RpcProxy.cs
@@ -1,181 +1,179 @@
-//------------------------------------------------------------------------------
-//  此代码版权（除特别声明或在XREF结尾的命名空间的代码）归作者本人若汝棋茗所有
-//  源代码使用协议遵循本仓库的开源协议及附加协议，若本仓库没有设置，则按MIT开源协议授权
-//  CSDN博客：https://blog.csdn.net/qq_40374647
-//  哔哩哔哩视频：https://space.bilibili.com/94253567
-//  Gitee源代码仓库：https://gitee.com/RRQM_Home
-//  Github源代码仓库：https://github.com/RRQM
-//  API首页：https://touchsocket.net/
-//  交流QQ群：234762506
-//  感谢您的下载和使用
-//------------------------------------------------------------------------------
-
 /*
 此代码由Rpc工具直接生成，非必要请不要修改此处代码
 */
 #pragma warning disable
 using System;
+using TouchSocket.Core;
+using TouchSocket.Sockets;
+using TouchSocket.Rpc;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
-using TouchSocket.Core;
-using TouchSocket.Rpc;
-using TouchSocket.Sockets;
 namespace RpcProxy
 {
-    public interface IXmlServer : TouchSocket.Rpc.IRemoteServer
-    {
-        ///<summary>
-        ///无注释信息
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        System.Int32 Sum(System.Int32 a, System.Int32 b, InvokeOption invokeOption = default);
-        ///<summary>
-        ///无注释信息
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        Task<System.Int32> SumAsync(System.Int32 a, System.Int32 b, InvokeOption invokeOption = default);
+public interface IXmlServer:TouchSocket.Rpc.IRemoteServer
+{
+///<summary>
+///无注释信息
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+[AsyncToSyncWarning]
+System.Int32 Sum(System.Int32 a,System.Int32 b,InvokeOption invokeOption = default);
+///<summary>
+///无注释信息
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+Task<System.Int32> SumAsync(System.Int32 a,System.Int32 b,InvokeOption invokeOption = default);
 
-        ///<summary>
-        ///无注释信息
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        System.Int32 TestClass(MyClass myClass, InvokeOption invokeOption = default);
-        ///<summary>
-        ///无注释信息
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        Task<System.Int32> TestClassAsync(MyClass myClass, InvokeOption invokeOption = default);
+///<summary>
+///无注释信息
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+[AsyncToSyncWarning]
+System.Int32 TestClass(MyClass myClass,InvokeOption invokeOption = default);
+///<summary>
+///无注释信息
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+Task<System.Int32> TestClassAsync(MyClass myClass,InvokeOption invokeOption = default);
 
-    }
-    public class XmlServer : IXmlServer
-    {
-        public XmlServer(IRpcClient client)
-        {
-            this.Client = client;
-        }
-        public IRpcClient Client { get; private set; }
-        ///<summary>
-        ///无注释信息
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        public System.Int32 Sum(System.Int32 a, System.Int32 b, InvokeOption invokeOption = default)
-        {
-            if (this.Client == null)
-            {
-                throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
-            }
-            object[] @_parameters = new object[] { a, b };
-            System.Int32 returnData = (System.Int32)this.Client.Invoke("Sum", typeof(System.Int32), invokeOption, @_parameters);
-            return returnData;
-        }
-        ///<summary>
-        ///无注释信息
-        ///</summary>
-        public async Task<System.Int32> SumAsync(System.Int32 a, System.Int32 b, InvokeOption invokeOption = default)
-        {
-            if (this.Client == null)
-            {
-                throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
-            }
-            object[] parameters = new object[] { a, b };
-            return (System.Int32)await this.Client.InvokeAsync("Sum", typeof(System.Int32), invokeOption, parameters);
-        }
+}
+public class XmlServer :IXmlServer
+{
+public XmlServer(IRpcClient client)
+{
+this.Client=client;
+}
+public IRpcClient Client{get;private set; }
+///<summary>
+///无注释信息
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+[AsyncToSyncWarning]
+public System.Int32 Sum(System.Int32 a,System.Int32 b,InvokeOption invokeOption = default)
+{
+if(this.Client==null)
+{
+throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
+}
+object[] @_parameters = new object[]{a,b};
+System.Int32 returnData=(System.Int32)this.Client.Invoke("Sum",typeof(System.Int32),invokeOption, @_parameters);
+return returnData;
 
-        ///<summary>
-        ///无注释信息
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        public System.Int32 TestClass(MyClass myClass, InvokeOption invokeOption = default)
-        {
-            if (this.Client == null)
-            {
-                throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
-            }
-            object[] @_parameters = new object[] { myClass };
-            System.Int32 returnData = (System.Int32)this.Client.Invoke("TestClass", typeof(System.Int32), invokeOption, @_parameters);
-            return returnData;
-        }
-        ///<summary>
-        ///无注释信息
-        ///</summary>
-        public async Task<System.Int32> TestClassAsync(MyClass myClass, InvokeOption invokeOption = default)
-        {
-            if (this.Client == null)
-            {
-                throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
-            }
-            object[] parameters = new object[] { myClass };
-            return (System.Int32)await this.Client.InvokeAsync("TestClass", typeof(System.Int32), invokeOption, parameters);
-        }
+}
+///<summary>
+///无注释信息
+///</summary>
+public async Task<System.Int32> SumAsync(System.Int32 a,System.Int32 b,InvokeOption invokeOption = default)
+{
+if(this.Client==null)
+{
+throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
+}
+object[] parameters = new object[]{a,b};
+return (System.Int32) await this.Client.InvokeAsync("Sum",typeof(System.Int32),invokeOption, parameters);
 
-    }
-    public static class XmlServerExtensions
-    {
-        ///<summary>
-        ///无注释信息
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        public static System.Int32 Sum<TClient>(this TClient client, System.Int32 a, System.Int32 b, InvokeOption invokeOption = default) where TClient :
-        TouchSocket.XmlRpc.IXmlRpcClient
-        {
-            object[] @_parameters = new object[] { a, b };
-            System.Int32 returnData = (System.Int32)client.Invoke("Sum", typeof(System.Int32), invokeOption, @_parameters);
-            return returnData;
-        }
-        ///<summary>
-        ///无注释信息
-        ///</summary>
-        public static async Task<System.Int32> SumAsync<TClient>(this TClient client, System.Int32 a, System.Int32 b, InvokeOption invokeOption = default) where TClient :
-        TouchSocket.XmlRpc.IXmlRpcClient
-        {
-            object[] parameters = new object[] { a, b };
-            return (System.Int32)await client.InvokeAsync("Sum", typeof(System.Int32), invokeOption, parameters);
-        }
+}
 
-        ///<summary>
-        ///无注释信息
-        ///</summary>
-        /// <exception cref="System.TimeoutException">调用超时</exception>
-        /// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
-        /// <exception cref="System.Exception">其他异常</exception>
-        public static System.Int32 TestClass<TClient>(this TClient client, MyClass myClass, InvokeOption invokeOption = default) where TClient :
-        TouchSocket.XmlRpc.IXmlRpcClient
-        {
-            object[] @_parameters = new object[] { myClass };
-            System.Int32 returnData = (System.Int32)client.Invoke("TestClass", typeof(System.Int32), invokeOption, @_parameters);
-            return returnData;
-        }
-        ///<summary>
-        ///无注释信息
-        ///</summary>
-        public static async Task<System.Int32> TestClassAsync<TClient>(this TClient client, MyClass myClass, InvokeOption invokeOption = default) where TClient :
-        TouchSocket.XmlRpc.IXmlRpcClient
-        {
-            object[] parameters = new object[] { myClass };
-            return (System.Int32)await client.InvokeAsync("TestClass", typeof(System.Int32), invokeOption, parameters);
-        }
+///<summary>
+///无注释信息
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+[AsyncToSyncWarning]
+public System.Int32 TestClass(MyClass myClass,InvokeOption invokeOption = default)
+{
+if(this.Client==null)
+{
+throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
+}
+object[] @_parameters = new object[]{myClass};
+System.Int32 returnData=(System.Int32)this.Client.Invoke("TestClass",typeof(System.Int32),invokeOption, @_parameters);
+return returnData;
 
-    }
-    public class MyClass
-    {
-        public System.Int32 A { get; set; }
-        public System.Int32 B { get; set; }
-    }
+}
+///<summary>
+///无注释信息
+///</summary>
+public async Task<System.Int32> TestClassAsync(MyClass myClass,InvokeOption invokeOption = default)
+{
+if(this.Client==null)
+{
+throw new RpcException("IRpcClient为空，请先初始化或者进行赋值");
+}
+object[] parameters = new object[]{myClass};
+return (System.Int32) await this.Client.InvokeAsync("TestClass",typeof(System.Int32),invokeOption, parameters);
+
+}
+
+}
+public static class XmlServerExtensions
+{
+///<summary>
+///无注释信息
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+[AsyncToSyncWarning]
+public static System.Int32 Sum<TClient>(this TClient client,System.Int32 a,System.Int32 b,InvokeOption invokeOption = default) where TClient:
+TouchSocket.XmlRpc.IXmlRpcClient{
+object[] @_parameters = new object[]{a,b};
+System.Int32 returnData=(System.Int32)client.Invoke("Sum",typeof(System.Int32),invokeOption, @_parameters);
+return returnData;
+
+}
+///<summary>
+///无注释信息
+///</summary>
+public static async Task<System.Int32> SumAsync<TClient>(this TClient client,System.Int32 a,System.Int32 b,InvokeOption invokeOption = default) where TClient:
+TouchSocket.XmlRpc.IXmlRpcClient{
+object[] parameters = new object[]{a,b};
+return (System.Int32) await client.InvokeAsync("Sum",typeof(System.Int32),invokeOption, parameters);
+
+}
+
+///<summary>
+///无注释信息
+///</summary>
+/// <exception cref="System.TimeoutException">调用超时</exception>
+/// <exception cref="TouchSocket.Rpc.RpcInvokeException">Rpc调用异常</exception>
+/// <exception cref="System.Exception">其他异常</exception>
+[AsyncToSyncWarning]
+public static System.Int32 TestClass<TClient>(this TClient client,MyClass myClass,InvokeOption invokeOption = default) where TClient:
+TouchSocket.XmlRpc.IXmlRpcClient{
+object[] @_parameters = new object[]{myClass};
+System.Int32 returnData=(System.Int32)client.Invoke("TestClass",typeof(System.Int32),invokeOption, @_parameters);
+return returnData;
+
+}
+///<summary>
+///无注释信息
+///</summary>
+public static async Task<System.Int32> TestClassAsync<TClient>(this TClient client,MyClass myClass,InvokeOption invokeOption = default) where TClient:
+TouchSocket.XmlRpc.IXmlRpcClient{
+object[] parameters = new object[]{myClass};
+return (System.Int32) await client.InvokeAsync("TestClass",typeof(System.Int32),invokeOption, parameters);
+
+}
+
+}
+public class MyClass
+{
+public System.Int32 A { get; set; }
+public System.Int32 B { get; set; }
+}
 
 }
