aae51b3810071ab3b27bcb026e7b53a782f82002
Sunny <53289193+yhuse@users.noreply.github.com>
2025-10-16 16:04:28 +0800

    修改 ConcurrentList.cs 一处错误代码，移除一处不必要的引用。 (#88)
    * Update ConcurrentList.cs
    
    修改错误的代码
    
    * Remove unused Newtonsoft.Json.Linq using directive
    
    移除不必要的引用

--- src/TouchSocket.Core/BitConverter/TouchSocketBitConverter.cs
+++ src/TouchSocket.Core/BitConverter/TouchSocketBitConverter.cs
@@ -10,7 +10,6 @@
 //  感谢您的下载和使用
 //------------------------------------------------------------------------------
 
-using Newtonsoft.Json.Linq;
 using System.Buffers;
 using System.Reflection;
 using System.Runtime.CompilerServices;
